ff1904d1390d3044c93b9f2c69501599d245be83
Atila Budai <atib1980@gmail.com>
2018-02-05 20:43:50 +0100

    small update

--- 3_hard_difficulty_level/37_max_heap_checker.cpp
+++ 3_hard_difficulty_level/37_max_heap_checker.cpp
@@ -30,56 +30,63 @@ using namespace std;
 
 void find_incorrect_max_heap_values(const int* arr, const size_t arr_size, const size_t pos, ostringstream& oss)
 {
-	if (pos >= arr_size) return;
+    if (pos >= arr_size)
+        return;
 
-	if (arr[pos] > arr[pos / 2]) oss << arr[pos] << ',';
+    if (arr[pos] > arr[pos / 2])
+        oss << arr[pos] << ',';
 
-	if ((pos + 1 < arr_size) && (arr[pos + 1] > arr[pos / 2])) oss << arr[pos + 1] << ',';
+    if ((pos + 1 < arr_size) && (arr[pos + 1] > arr[pos / 2]))
+        oss << arr[pos + 1] << ',';
 
-	if (2 * pos + 1 < arr_size) find_incorrect_max_heap_values(arr, arr_size, 2 * pos + 1, oss);
+    if (2 * pos + 1 < arr_size)
+        find_incorrect_max_heap_values(arr, arr_size, 2 * pos + 1, oss);
 
-	if (2 * (pos + 1) + 1 < arr_size) find_incorrect_max_heap_values(arr, arr_size, 2 * (pos + 1) + 1, oss);
+    if (2 * pos + 3 < arr_size)
+        find_incorrect_max_heap_values(arr, arr_size, 2 * pos + 3, oss);
 }
 
 string MaxHeapChecker(const int* arr, const size_t arr_size)
 {
-	if (1u == arr_size) return "max";
+    if (1u == arr_size)
+        return "max";
 
-	ostringstream oss{};
+    ostringstream oss{};
 
-	find_incorrect_max_heap_values(arr, arr_size, 1, oss);
+    find_incorrect_max_heap_values(arr, arr_size, 1, oss);
 
-	string result{oss.str()};
+    string result{ oss.str() };
 
-	if (result.empty()) return "max";
+    if (result.empty())
+        return "max";
 
-	result.erase(--end(result));
+    result.erase(--end(result));
 
-	return result;
+    return result;
 }
 
 int main()
 {
-	// const int A[] = gets(stdin);
-	// cout << MaxHeapChecker(A, sizeof(A)/sizeof(*A));
-	const int B[] = {100,19,36,17};
-	cout << MaxHeapChecker(B, sizeof(B) / sizeof(*B)) << '\n'; // expected output: "max"
-	const int C[] = {10,19,52,13,16};
-	cout << MaxHeapChecker(C, sizeof(C) / sizeof(*C)) << '\n'; // expected output: "19,52"
-	const int D[] = {10,19,52,104,14};
-	cout << MaxHeapChecker(D, sizeof(D) / sizeof(*D)) << '\n'; // expected output: "19,52,104"
-	const int E[] = {73,74,75,7,2,107};
-	cout << MaxHeapChecker(E, sizeof(E) / sizeof(*E)) << '\n'; // expected output: "74,75,107"
-	const int F[] = {1,5,10,2,3,10,1};
-	cout << MaxHeapChecker(F, sizeof(F) / sizeof(*F)) << '\n'; // expected output: "5,10"
-	const int G[] = {5,2,1,1,1};
-	cout << MaxHeapChecker(G, sizeof(G) / sizeof(*G)) << '\n'; // expected output: "max"
-	const int H[] = {73,72,1,79,90};
-	cout << MaxHeapChecker(H, sizeof(H) / sizeof(*H)) << '\n'; // expected output: "79,90"
-	const int I[] = {100,99,98,97,96,95,94};
-	cout << MaxHeapChecker(I, sizeof(I) / sizeof(*I)) << '\n'; // expected output: "max"
-	const int J[] = {73,74,81,79,90};
-	cout << MaxHeapChecker(J, sizeof(J) / sizeof(*J)) << '\n'; // expected output: "74,81,79,90"
+    // const int A[] = gets(stdin);
+    // cout << MaxHeapChecker(A, sizeof(A)/sizeof(*A));
+    const int B[] = { 100, 19, 36, 17 };
+    cout << MaxHeapChecker(B, sizeof(B) / sizeof(*B)) << '\n'; // expected output: "max"
+    const int C[] = { 10, 19, 52, 13, 16 };
+    cout << MaxHeapChecker(C, sizeof(C) / sizeof(*C)) << '\n'; // expected output: "19,52"
+    const int D[] = { 10, 19, 52, 104, 14 };
+    cout << MaxHeapChecker(D, sizeof(D) / sizeof(*D)) << '\n'; // expected output: "19,52,104"
+    const int E[] = { 73, 74, 75, 7, 2, 107 };
+    cout << MaxHeapChecker(E, sizeof(E) / sizeof(*E)) << '\n'; // expected output: "74,75,107"
+    const int F[] = { 1, 5, 10, 2, 3, 10, 1 };
+    cout << MaxHeapChecker(F, sizeof(F) / sizeof(*F)) << '\n'; // expected output: "5,10"
+    const int G[] = { 5, 2, 1, 1, 1 };
+    cout << MaxHeapChecker(G, sizeof(G) / sizeof(*G)) << '\n'; // expected output: "max"
+    const int H[] = { 73, 72, 1, 79, 90 };
+    cout << MaxHeapChecker(H, sizeof(H) / sizeof(*H)) << '\n'; // expected output: "79,90"
+    const int I[] = { 100, 99, 98, 97, 96, 95, 94 };
+    cout << MaxHeapChecker(I, sizeof(I) / sizeof(*I)) << '\n'; // expected output: "max"
+    const int J[] = { 73, 74, 81, 79, 90 };
+    cout << MaxHeapChecker(J, sizeof(J) / sizeof(*J)) << '\n'; // expected output: "74,81,79,90"
 
-	return 0;
+    return 0;
 }
