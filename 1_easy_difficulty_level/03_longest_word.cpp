2458228869a89220bfb9330d0b22eaa9cea1f779
Atila Budai <atib1980@gmail.com>
2018-02-09 21:03:49 +0100

    update

--- 1_easy_difficulty_level/03_longest_word.cpp
+++ 1_easy_difficulty_level/03_longest_word.cpp
@@ -1,9 +1,10 @@
 /*
 Coderbyte coding challenge: Longest Word
 
-Using the C++ language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. 
-If there are two or more words that are the same length, return the first word from the string with that length. 
-Ignore punctuation and assume sen will not be empty.
+Using the C++ language, have the function LongestWord(sen) take the sen
+parameter being passed and return the largest word in the string. If there are
+two or more words that are the same length, return the first word from the
+string with that length. Ignore punctuation and assume sen will not be empty.
 
 Sample test cases:
 
@@ -14,62 +15,77 @@ Input:  "I love dogs"
 Output: "love"
 */
 
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <cctype>
-#include <map>
 
 using namespace std;
 
-string LongestWord(string sen) { 
-    
-    size_t current_max_len{}, si{string::npos};
-   
-    map<size_t, string> words{};
+string trim(const string& str) {
+  const size_t str_len{str.length()};
 
-    for (size_t i{}; i < sen.length(); i++) {
-       
-        if (isalnum(sen[i])) {
+  if (0u == str.length())
+    return string{};
 
-        	if (string::npos == si) si = i;
+  size_t begin_str{};
+  size_t end_str{str_len - 1};
 
-            current_max_len++;
-            continue;
-        }
-
-        if ((words.find(current_max_len) == end(words)) && (string::npos != si)) {
-
-        	words[current_max_len] = sen.substr(si, current_max_len);
-    	}
-
-    	si = string::npos;
-      current_max_len = 0u;
-
-    }
-
-    if (current_max_len && (string::npos != si)) {
-
-    	if (words.find(current_max_len) == end(words)) {
-
-        	words[current_max_len] = sen.substr(si, current_max_len);
-    	}
-    }
-    
-
-  if (!words.empty()) {
-  	auto last_pair_pos = end(words);
-  	--last_pair_pos;
-  	return last_pair_pos->second;  
+  for (; begin_str <= end_str; ++begin_str) {
+    if (!isspace(str[begin_str]))
+      break;
   }
 
-  return string{}; 
-            
+  if (begin_str > end_str)
+    return string{};
+
+  for (; end_str > begin_str; --end_str) {
+    if (!isspace(str[end_str]))
+      break;
+  }
+
+  return str.substr(begin_str, end_str - begin_str + 1);
 }
 
-int main() { 
-  
+string LongestWord(string sen) {
+  size_t word_len{}, start_index{string::npos};
+  size_t max_word_len{};
+  string longest_first_word{};
+
+  sen = trim(sen);
+
+  for (size_t i{}; i < sen.length(); i++) {
+    if (isalnum(sen[i])) {
+      if (string::npos == start_index)
+        start_index = i;
+      word_len++;
+      continue;
+    }
+
+    if ((word_len > max_word_len) && (string::npos != start_index)) {
+      max_word_len = word_len;
+      longest_first_word = sen.substr(start_index, word_len);
+    } else if (!word_len)
+      continue;
+
+    start_index = string::npos;
+    word_len = 0u;
+  }
+
+  if ((word_len > max_word_len) && (string::npos != start_index)) {
+    longest_first_word = sen.substr(start_index, word_len);
+  }
+
+  return longest_first_word;
+}
+
+int main() {
   // cout << LongestWord(gets(stdin));
-  cout << LongestWord(move(string{"fun&!! time"})) << '\n'; // expected output: "time"
-  cout << LongestWord(move(string{"I love dogs"})) << '\n'; // expected output: "love"
-  return 0;    
+  cout << LongestWord(move(string{"fun&!! time"}))
+       << '\n';  // expected output: "time"
+  cout << LongestWord(move(string{"I love dogs"}))
+       << '\n';  // expected output: "love"
+  cout << LongestWord(move(string{"I love both cats and dogs as well!"}))
+       << '\n';  // expected output: "love"
+
+  return 0;
 }
