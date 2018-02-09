f6cc796bbbe571fa022f4ff4ccbd87a02a67da18
Atila Budai <atib1980@gmail.com>
2018-02-09 22:09:47 +0100

    code improvement

--- 1_easy_difficulty_level/09_time_convert.cpp
+++ 1_easy_difficulty_level/09_time_convert.cpp
@@ -1,51 +1,38 @@
 /*
-Coderbyte coding challenge: Time Convert
+ Coderbyte coding challenge: Time Convert
 
-Using the C++ language, have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes the parameter converts to 
-(ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon.
+ Using the C++ language, have the function TimeConvert(num) take the num
+ parameter being passed and return the number of hours and minutes the parameter
+ converts to (ie. if num = 63 then the output should be 1:3). Separate the
+ number of hours and minutes with a colon.
 
-Sample test cases:
+ Sample test cases:
 
-Input:  126
-Output: "2:6"
+ Input:  126
+ Output: "2:6"
 
-Input:  45
-Output: "0:45"
-*/
+ Input:  45
+ Output: "0:45"
+ */
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-string TimeConvert(const int num) { 
+string TimeConvert(const size_t minutes) {
 
-  string final{};
+  if (minutes < 60) 
+    return string{"0:"} + to_string(minutes);
   
-  if (num < 60) {
-      
-      final = "0:";
-      final += std::to_string(num);
-      return final;
-  }
-  
-  int hours = num / 60;
-  int minutes = num - (hours * 60);
-  
-  final += to_string(hours);
-  final += ":";
-  final += to_string(minutes);
-
-  return final;
-            
+  return to_string(minutes / 60) + string{":"} + to_string(minutes % 60);
 }
 
-int main() { 
-
+int main() {
   // cout << TimeConvert(gets(stdin));
-  cout << TimeConvert(120) << '\n'; // expected output: "2:0"
-  cout << TimeConvert(35) << '\n';  // expected output: "0:35"
-  cout << TimeConvert(256) << '\n'; // expected output: "4:16"
+  cout << TimeConvert(120) << '\n';  // expected output: "2:0"
+  cout << TimeConvert(35) << '\n';   // expected output: "0:35"
+  cout << TimeConvert(256) << '\n';  // expected output: "4:16"
+
   return 0;
-    
 }
