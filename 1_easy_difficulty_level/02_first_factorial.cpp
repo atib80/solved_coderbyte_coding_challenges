005d0b78308255400b32156647a3c95ed03cfdf1
Atila Budai <atib1980@gmail.com>
2018-02-19 17:16:33 +0100

    added missing description

--- 1_easy_difficulty_level/02_first_factorial.cpp
+++ 1_easy_difficulty_level/02_first_factorial.cpp
@@ -1,3 +1,20 @@
+/*
+Coderbyte coding challenge: First Factorial
+
+Using the C++ language, have the function FirstFactorial(num) take the num
+parameter being passed and return the factorial of it (e.g. if num = 4, return
+(4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the
+input will always be an integer.
+
+Sample test cases:
+
+Input:  4
+Output: 24
+
+Input:  8
+Output: 40320
+*/
+
 #include <cstdint>
 #include <iostream>
 
