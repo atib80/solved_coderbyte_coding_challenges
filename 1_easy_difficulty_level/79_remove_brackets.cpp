5637d025f91d735224221ecf4e866774af728760
Atila Budai <atib1980@gmail.com>
2018-02-25 22:21:24 +0100

    code improvement

--- 1_easy_difficulty_level/79_remove_brackets.cpp
+++ 1_easy_difficulty_level/79_remove_brackets.cpp
@@ -1,9 +1,11 @@
 /*
 Coding challenge: Remove Brackets
 
-Using the C++ language, have the function RemoveBrackets(str) take the str string parameter being passed, which will contain only the characters "(" and ")", 
-and determine the minimum number of brackets that need to be removed to create a string of correctly matched brackets. 
-For example: if str is "(()))" then your program should return the number 1. The answer could potentially be 0, 
+Using the C++ language, have the function RemoveBrackets(str) take the str
+string parameter being passed, which will contain only the characters "(" and
+")", and determine the minimum number of brackets that need to be removed to
+create a string of correctly matched brackets. For example: if str is "(()))"
+then your program should return the number 1. The answer could potentially be 0,
 and there will always be at least one set of matching brackets in the string.
 
 Sample test cases:
@@ -15,74 +17,73 @@ Input:  "(()("
 Output: 2
 */
 
-#include <iostream>
-#include <string>
 #include <cctype>
 #include <cmath>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-string trim(const string& str)
-{
-	size_t begin_str{};
-	size_t end_str{str.size() - 1};
+string trim(const string& str) {
+  const size_t str_len{str.length()};
 
-	if (0u == str.length()) return string{};
+  if (!str_len)
+    return string{};
 
-	for (; begin_str <= end_str; ++begin_str)
-	{
-		if (!isspace(str[begin_str])) break;
-	}
+  size_t first{}, last{str_len - 1};
 
-	if (begin_str > end_str) return string{};
+  for (; first <= last; ++first) {
+    if (!isspace(str[first]))
+      break;
+  }
 
-	for (; end_str > begin_str; --end_str)
-	{
-		if (!isspace(str[end_str])) break;
-	}
+  if (first > last)
+    return string{};
 
-	return str.substr(begin_str, end_str - begin_str + 1);
+  for (; last > first; --last) {
+    if (!isspace(str[last]))
+      break;
+  }
+
+  return str.substr(first, last - first + 1);
 }
 
-string RemoveBrackets(string str) { 
-
+string RemoveBrackets(string str) {
   str = trim(str);
 
   int balance_count{};
 
-  size_t start{0u};
+  size_t start{};
 
   while (')' == str[start]) {
-  	start++;
-  	balance_count++;
+    start++;
+    balance_count++;
   }
 
   size_t last{str.length() - 1};
 
   while ('(' == str[last]) {
-  	last--;
-  	balance_count++;
+    last--;
+    balance_count++;
   }
 
   for (size_t i{start}; i <= last; i++) {
-  	
-  	if ('(' == str[i]) balance_count++;
- 	  else if (')' == str[i]) balance_count--;  	
-  
+    if ('(' == str[i])
+      balance_count++;
+    else if (')' == str[i])
+      balance_count--;
   }
 
-  return to_string(abs(balance_count)); 
-            
+  return to_string(abs(balance_count));
 }
 
-int main() { 
-  
+int main() {
   // cout << RemoveBrackets(move(string{gets(stdin)}));
-  cout << RemoveBrackets(move(string{"(()))"})) << '\n';     //  expected output: 1
-  cout << RemoveBrackets(move(string{"(())()((("})) << '\n'; //  expected output: 3
-  cout << RemoveBrackets(move(string{"(()("})) << '\n';      //  expected output: 2
-  cout << RemoveBrackets(move(string{")(()"})) << '\n';      //  expected output: 2
-  
+  cout << RemoveBrackets(move(string{"(()))"})) << '\n';  //  expected output: 1
+  cout << RemoveBrackets(move(string{"(())()((("}))
+       << '\n';                                          //  expected output: 3
+  cout << RemoveBrackets(move(string{"(()("})) << '\n';  //  expected output: 2
+  cout << RemoveBrackets(move(string{")(()"})) << '\n';  //  expected output: 2
+
   return 0;
-    
 }
