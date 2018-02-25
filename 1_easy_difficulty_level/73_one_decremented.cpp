aedfdf08a9ab82e32252a7030a196abe0d043b95
Atila Budai <atib1980@gmail.com>
2018-02-25 20:11:22 +0100

    code improvement

--- 1_easy_difficulty_level/73_one_decremented.cpp
+++ 1_easy_difficulty_level/73_one_decremented.cpp
@@ -1,9 +1,11 @@
 /*
 Coderbyte coding challenge: One Decremented
 
-Using the C++ language, have the function OneDecremented(num) count how many times a digit appears that is exactly one less than the previous digit. 
-For example: if num is 5655984 then your program should return 2 because 5 appears directly after 6 and 8 appears directly after 9. 
-The input will always contain at least 1 digit.
+Using the C++ language, have the function OneDecremented(num) count how many
+times a digit appears that is exactly one less than the previous digit. For
+example: if num is 5655984 then your program should return 2 because 5 appears
+directly after 6 and 8 appears directly after 9. The input will always contain
+at least 1 digit.
 
 Sample test cases:
 
@@ -14,36 +16,33 @@ Input:  9876541110
 Output: 6
 */
 
-#include <iostream>
 #include <cstdint>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-size_t OneDecremented(const int64_t num) { 
+size_t OneDecremented(const int64_t num) {
+  const string num_str{to_string(num)};
 
-  const string num_str{ to_string(num) };
-
-  if (num_str.length() < 2u) return 0u;
+  if (num_str.length() < 2)
+    return 0;
 
   size_t freq{};
 
-  for (size_t i{}; i != num_str.length() - 1; i++) {
+  for (size_t i{}; i < num_str.length() - 1; i++) {
+    if (1 == static_cast<int>(num_str[i] - num_str[i + 1]))
+      freq++;
+  }
 
-  	if ((num_str[i+1] < num_str[i]) && (1u == static_cast<size_t>(num_str[i] - num_str[i+1]))) freq++;
-
-  }  
-  
   return freq;
-
 }
 
-int main() { 
-  
+int main() {
   // cout << OneDecremented(gets(stdin));
   cout << OneDecremented(5655984LL) << '\n';     // expected output: 2
   cout << OneDecremented(56LL) << '\n';          // expected output: 0
   cout << OneDecremented(9876541110LL) << '\n';  // expected output: 6
 
-  return 0;    
-} 
+  return 0;
+}
--- 1_easy_difficulty_level/75_gcf.cpp
+++ 1_easy_difficulty_level/75_gcf.cpp
@@ -1,9 +1,11 @@
 /*
 Coderbyte coding challenge: GCF
 
-Using the C++ language, have the function GCF(arr) take the array of numbers stored in arr which will always contain only two positive integers, 
-and return the greatest common factor of them. For example: if arr is [45, 12] then your program should return 3. 
-There will always be two elements in the array and they will be positive integers.
+Using the C++ language, have the function GCF(arr) take the array of numbers
+stored in arr which will always contain only two positive integers, and return
+the greatest common factor of them. For example: if arr is [45, 12] then your
+program should return 3. There will always be two elements in the array and they
+will be positive integers.
 
 Sample test cases:
 
@@ -14,38 +16,37 @@ Input:  12, 28
 Output: 4
 */
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 int GCF(const int* arr, const size_t arr_size) {
+  if (!arr)
+    return 0;
 
-	if (arr_size < 2u) return -1;
+  if (arr_size < 2)
+    return *arr;
 
-	const int min_number { min(arr[0], arr[1]) };
+  const int min_number{min(arr[0], arr[1])};
 
-	for (int i { min_number }; i >= 1; i--) {
+  for (int gcf{min_number}; gcf > 1; gcf--) {
+    if ((arr[0] % gcf == 0) && (arr[1] % gcf == 0))
+      return gcf;
+  }
 
-		if ((arr[0] % i == 0) && (arr[1] % i == 0)) return i;
-
-	}
-
-  return 1; 
-            
+  return 1;
 }
 
-int main() { 
-   
+int main() {
   // const int A[] = gets(stdin);
   // cout << GCF(A, sizeof(A)/sizeof(*A));
   const int B[] = {45, 12};
-  cout << GCF(B, sizeof(B)/sizeof(*B)) << '\n'; // expected output: 3
+  cout << GCF(B, sizeof(B) / sizeof(*B)) << '\n';  // expected output: 3
   const int C[] = {1, 6};
-  cout << GCF(C, sizeof(C)/sizeof(*C)) << '\n'; // expected output: 1
+  cout << GCF(C, sizeof(C) / sizeof(*C)) << '\n';  // expected output: 1
   const int D[] = {12, 28};
-  cout << GCF(D, sizeof(D)/sizeof(*D)) << '\n'; // expected output: 4
+  cout << GCF(D, sizeof(D) / sizeof(*D)) << '\n';  // expected output: 4
 
   return 0;
-    
 }
