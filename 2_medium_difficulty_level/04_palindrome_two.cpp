bcadea07b2f118ce6c4952350b37494bd3ec2d8b
Atila Budai <atib1980@gmail.com>
2018-02-26 21:52:19 +0100

    code improvement

--- 2_medium_difficulty_level/04_palindrome_two.cpp
+++ 2_medium_difficulty_level/04_palindrome_two.cpp
@@ -1,9 +1,12 @@
 /*
 Coderbyte coding challenge: Palindrome Two
 
-Using the C++ language, have the function PalindromeTwo(str) take the str parameter being passed and return the string true if the parameter is a palindrome, 
-(the string is the same forward as it is backward) otherwise return the string false. The parameter entered may have punctuation and symbols but they should 
-not affect whether the string is in fact a palindrome. For example: "Anne, I vote more cars race Rome-to-Vienna" should return true.
+Using the C++ language, have the function PalindromeTwo(str) take the str
+parameter being passed and return the string true if the parameter is a
+palindrome, (the string is the same forward as it is backward) otherwise return
+the string false. The parameter entered may have punctuation and symbols but
+they should not affect whether the string is in fact a palindrome. For example:
+"Anne, I vote more cars race Rome-to-Vienna" should return true.
 
 Sample test cases:
 
@@ -14,73 +17,112 @@ Input:  "A war at Tarawa!"
 Output: "true"
 */
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <locale>
 #include <sstream>
 #include <string>
-#include <cctype>
-#include <algorithm>
 
 using namespace std;
 
-string trim(const string& str)
-{
-	size_t begin_str{};
-	size_t end_str{str.size() - 1};
+string trim(const string& str) {
+  const size_t str_len{str.length()};
 
-	if (0u == str.length()) return string{};
+  if (!str_len)
+    return string{};
 
-	for (; begin_str <= end_str; ++begin_str)
-	{
-		if (!isspace(str[begin_str])) break;
-	}
+  size_t first{}, last{str_len - 1};
 
-	if (begin_str > end_str) return string{};
+  for (; first <= last; ++first) {
+    if (!isspace(str[first]))
+      break;
+  }
 
-	for (; end_str > begin_str; --end_str)
-	{
-		if (!isspace(str[end_str])) break;
-	}
+  if (first > last)
+    return string{};
 
-	return str.substr(begin_str, end_str - begin_str + 1);
+  for (; last > first; --last) {
+    if (!isspace(str[last]))
+      break;
+  }
+
+  return str.substr(first, last - first + 1);
 }
 
-string PalindromeTwo(string str) { 
-
+string PalindromeTwo_v1(string str, const locale& loc = locale{}) {
   str = trim(str);
 
-  const size_t str_len { str.length() };
+  const size_t str_len{str.length()};
 
-  if (1u == str_len) return "false";
+  if (1 == str_len)
+    return "false";
 
-  if ((2u == str_len) && (str[0] == str[1])) return "true";
+  if (2 == str_len && str[0] == str[1])
+    return "true";
 
   ostringstream oss{};
 
   for (const char c : str) {
-
-    if (isalnum(c)) oss << static_cast<char>(tolower(c));
-
+    if (isalnum(c))
+      oss << static_cast<char>(tolower(c, loc));
   }
 
-  const string str1 { oss.str() };
+  const string str1{oss.str()};
 
-  string str2 { str1 };
+  string str2{str1};
 
   reverse(begin(str2), end(str2));
 
-  if (str1 == str2) return "true";
+  if (str1 == str2)
+    return "true";
 
   return "false";
-            
 }
 
-int main() { 
-  
-  // cout << PalindromeTwo(move(string{gets(stdin)}));
-  cout << PalindromeTwo(move(string{"Anne, I vote more cars race Rome-to-Vienna"})) << '\n'; // expected: "true"
-  cout << PalindromeTwo(move(string{"Noel - sees Leon"})) << '\n'; 							             // expected: "true"
-  cout << PalindromeTwo(move(string{"A war at Tarawa!"})) << '\n'; 							             // expected: "true"
+string PalindromeTwo_v2(string str, const locale& loc = locale{}) {
+  str = trim(str);
+
+  const size_t str_len{str.length()};
+
+  if (1 == str_len)
+    return "false";
+
+  if (2 == str_len && str[0] == str[1])
+    return "true";
+
+  size_t i{}, j{str_len - 1};
+
+  while (i < j) {
+    if (!isalnum(str[i])) {
+      i++;
+      continue;
+    }
+
+    if (!isalnum(str[j])) {
+      j--;
+      continue;
+    }
+
+    if (tolower(str[i], loc) != tolower(str[j], loc))
+      return "false";
+
+    i++;
+    j--;
+  }
+
+  return "true";
+}
+
+int main() {
+  // cout << PalindromeTwo_v2(move(string{gets(stdin)}));
+  cout << PalindromeTwo_v2(
+              move(string{"Anne, I vote more cars race Rome-to-Vienna"}))
+       << '\n';  // expected: "true"
+  cout << PalindromeTwo_v2(move(string{"Noel - sees Leon"}))
+       << '\n';  // expected: "true"
+  cout << PalindromeTwo_v2(move(string{"A war at Tarawa!"}))
+       << '\n';  // expected: "true"
 
   return 0;
-    
 }
