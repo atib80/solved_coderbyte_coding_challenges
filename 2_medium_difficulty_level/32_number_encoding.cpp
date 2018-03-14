02aab85e764e0f295cac5d2e9ce3b4a0bc48d1f7
Atila Budai <atib1980@gmail.com>
2018-03-14 02:09:07 +0100

    code improvement, alt. solution

--- 2_medium_difficulty_level/32_number_encoding.cpp
+++ 2_medium_difficulty_level/32_number_encoding.cpp
@@ -1,9 +1,11 @@
 /*
 Coderbyte coding challenge: Number Encoding
 
-Using the C++ language, have the function NumberEncoding(str) take the str parameter and encode the message according to the following rule: 
-encode every letter into its corresponding numbered position in the alphabet. Symbols and spaces will also be used in the input. 
-For example: if str is "af5c a#!" then your program should return 1653 1#!.
+Using the C++ language, have the function NumberEncoding(str) take the str
+parameter and encode the message according to the following rule: encode every
+letter into its corresponding numbered position in the alphabet. Symbols and
+spaces will also be used in the input. For example: if str is "af5c a#!" then
+your program should return 1653 1#!.
 
 Sample test cases:
 
@@ -14,41 +16,64 @@ Input:  "jaj-a"
 Output: 10110-1
 */
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
 
-string NumberEncoding(string str) {
+string trim(const string& input) {
+  string output{input};
+  output.erase(begin(output),
+               find_if(begin(output), end(output),
+                       [](const char ch) { return !isspace(ch); }));
 
-	static const string alphabet_str{"abcdefghijklmnopqrstuvwxyz"};
+  output.erase(find_if(output.rbegin(), output.rend(),
+                       [](const char ch) { return !isspace(ch); })
+                   .base(),
+               end(output));
 
-	string encoded_str{};
-
-	for (size_t i{}; i < str.length(); i++) {
-
-		const size_t index {alphabet_str.find(str[i])};
-
-		if (string::npos == index) {
-			encoded_str.push_back(str[i]);
-			continue;
-		}
-
-		encoded_str.append(to_string(index + 1u)); 
-	}
-  
-    return encoded_str;             
+  return output;
 }
 
-int main() { 
-  
-  // cout << NumberEncoding(move(string{gets(stdin)}));
-  cout << NumberEncoding(move(string{"af5c a#!"})) << '\n'; // expected output: 1653 1#!
-  cout << NumberEncoding(move(string{"hello 45"})) << '\n'; // expected output: 85121215 45
-  cout << NumberEncoding(move(string{"jaj-a"})) << '\n';    // expected output: 10110-1
-  cout << NumberEncoding(move(string{"km#e"})) << '\n';     // expected output: 1113#5
-  cout << NumberEncoding(move(string{"af5c a#!"})) << '\n'; // expected output: 1653 1#!
-  cout << NumberEncoding(move(string{"---fc9#"})) << '\n';  // expected output: ---639#
+string NumberEncoding(string str) {
+  static const string alphabet_str{"abcdefghijklmnopqrstuvwxyz"};
 
-  return 0;    
+  str = trim(str);
+
+  ostringstream oss{};
+
+  for (size_t i{}; i < str.length(); i++) {
+    str[i] = static_cast<char>(tolower(str[i]));
+    const size_t index{alphabet_str.find(str[i])};
+
+    if (string::npos == index) {
+      oss << str[i];
+      continue;
+    }
+
+    oss << index + 1;
+  }
+
+  return oss.str();
+}
+
+int main() {
+  // cout << NumberEncoding(move(string{gets(stdin)}));
+  cout << NumberEncoding(move(string{"af5c a#!"}))
+       << '\n';  // expected output: 1653 1#!
+  cout << NumberEncoding(move(string{"hello 45"}))
+       << '\n';  // expected output: 85121215 45
+  cout << NumberEncoding(move(string{"jaj-a"}))
+       << '\n';  // expected output: 10110-1
+  cout << NumberEncoding(move(string{"km#e"}))
+       << '\n';  // expected output: 1113#5
+  cout << NumberEncoding(move(string{"af5c a#!"}))
+       << '\n';  // expected output: 1653 1#!
+  cout << NumberEncoding(move(string{"---fc9#"}))
+       << '\n';  // expected output: ---639#
+
+  return 0;
 }
--- 2_medium_difficulty_level/33_stock_picker.cpp
+++ 2_medium_difficulty_level/33_stock_picker.cpp
@@ -1,16 +1,19 @@
 /*
 Coderbyte coding challenge: Stock Picker
 
-Using the C++ language, have the function StockPicker(arr) take the array of numbers stored in arr which will contain integers
-that represent the amount in dollars that a single stock is worth, and return the maximum profit 
-that could have been made by buying stock on day x and selling stock on day y where y > x. 
-For example: if arr is [44, 30, 24, 32, 35, 30, 40, 38, 15] then your program should return 16 
-because at index 2 the stock was worth $24 and at index 6 the stock was then worth $40, 
-so if you bought the stock at 24 and sold it at 40, you would have made a profit of $16, 
-which is the maximum profit that could have been made with this list of stock prices.
+Using the C++ language, have the function StockPicker(arr) take the array of
+numbers stored in arr which will contain integers that represent the amount in
+dollars that a single stock is worth, and return the maximum profit that could
+have been made by buying stock on day x and selling stock on day y where y > x.
+For example: if arr is [44, 30, 24, 32, 35, 30, 40, 38, 15] then your program
+should return 16 because at index 2 the stock was worth $24 and at index 6 the
+stock was then worth $40, so if you bought the stock at 24 and sold it at 40,
+you would have made a profit of $16, which is the maximum profit that could have
+been made with this list of stock prices.
 
-If there is not profit that could have been made with the stock prices, then your program should return -1. 
-For example: arr is [10, 9, 8, 2] then your program should return -1.
+If there is not profit that could have been made with the stock prices, then
+your program should return -1. For example: arr is [10, 9, 8, 2] then your
+program should return -1.
 
 Sample test cases:
 
@@ -21,42 +24,70 @@ Input:  14,20,4,12,5,11
 Output: 8
 */
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int StockPicker(const int* arr, const size_t arr_size) {
+int StockPicker_v1(const int* arr, const int arr_size) {
+  if (arr_size < 2)
+    return -1;
 
-  if (arr_size < 2u) return -1;
-
-  int current_max_profit{};  
+  int current_max_profit{};
   int current_minimum{arr[0]};
 
-  for (size_t i{}; i != arr_size - 1; i++) {
+  for (int i{1}; i < arr_size; i++) {
+    if (arr[i] - current_minimum > current_max_profit)
+      current_max_profit = arr[i] - current_minimum;
 
-  	if ((arr[i+1] - current_minimum) > current_max_profit) current_max_profit = arr[i+1] - current_minimum;
-
-  	else if (arr[i+1] < current_minimum) current_minimum = arr[i+1];
-  
+    else if (arr[i] < current_minimum)
+      current_minimum = arr[i];
   }
 
-  if (!current_max_profit) return -1;
+  if (!current_max_profit)
+    return -1;
 
   return current_max_profit;
-  
 }
 
-int main() {   
-   
+int StockPicker_v2(const int* arr, const int arr_size) {
+  if (arr_size < 2)
+    return -1;
+
+  vector<pair<int, int>> numbers(arr_size);
+
+  for (int i{}; i < arr_size; i++)
+    numbers[i] = make_pair(arr[i], i);
+
+  sort(begin(numbers), end(numbers),
+       [](const pair<int, int>& lp, const pair<int, int>& rp) {
+         return lp.first > rp.first;
+       });
+
+  for (int i{}; i < arr_size - 1; i++) {
+    for (int j{arr_size - 1}; j > i; j--) {
+      if (numbers[i].second > numbers[j].second)
+        return numbers[i].first - numbers[j].first;
+    }
+  }
+
+  return -1;
+}
+
+int main() {
   // const int A[] = gets(stdin);
-  // cout << StockPicker(A, sizeof(A)/sizeof(*A));
+  // cout << StockPicker_v1(A, sizeof(A)/sizeof(*A));
   const int B[] = {44, 30, 24, 32, 35, 30, 40, 38, 15};
-  cout << StockPicker(B, sizeof(B)/sizeof(*B)) << '\n'; // expected output: 16
-  const int C[] = {10,12,4,5,9};
-  cout << StockPicker(C, sizeof(C)/sizeof(*C)) << '\n'; // expected output: 5
-  const int D[] = {14,20,4,12,5,11};
-  cout << StockPicker(D, sizeof(D)/sizeof(*D)) << '\n'; // expected output: 8
+  cout << StockPicker_v1(B, sizeof(B) / sizeof(*B))
+       << '\n';  // expected output: 16
+  const int C[] = {10, 12, 4, 5, 9};
+  cout << StockPicker_v1(C, sizeof(C) / sizeof(*C))
+       << '\n';  // expected output: 5
+  const int D[] = {14, 20, 4, 12, 5, 11};
+  cout << StockPicker_v1(D, sizeof(D) / sizeof(*D))
+       << '\n';  // expected output: 8
 
   return 0;
 }
