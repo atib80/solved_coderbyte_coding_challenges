f4d87c2aeab6732b696e942c322c4e7eabbd4bff
Atila Budai <atib1980@gmail.com>
2018-03-19 18:47:37 +0100

    couple of small updates, code formatting

--- 2_medium_difficulty_level/45_matrix_spiral.cpp
+++ 2_medium_difficulty_level/45_matrix_spiral.cpp
@@ -1,16 +1,19 @@
 /*
 Coderbyte coding challenge: Matrix Spiral
 
-Using the C++ language, have the function MatrixSpiral(strArr) read the array of strings stored in strArr which will represent a 2D N matrix, 
-and your program should return the elements after printing them in a clockwise, spiral order. 
-You should return the newly formed list of elements as a string with the numbers separated by commas. 
-For example: if strArr is "[1, 2, 3]", "[4, 5, 6]", "[7, 8, 9]" then this looks like the following 2D matrix:
+Using the C++ language, have the function MatrixSpiral(strArr) read the array of
+strings stored in strArr which will represent a 2D N matrix, and your program
+should return the elements after printing them in a clockwise, spiral order. You
+should return the newly formed list of elements as a string with the numbers
+separated by commas. For example: if strArr is "[1, 2, 3]", "[4, 5, 6]", "[7, 8,
+9]" then this looks like the following 2D matrix:
 
 1 2 3
 4 5 6
 7 8 9
 
-So your program should return the elements of this matrix in a clockwise, spiral order which is: 1,2,3,6,9,8,7,4,5
+So your program should return the elements of this matrix in a clockwise, spiral
+order which is: 1,2,3,6,9,8,7,4,5
 
 Sample test cases:
 
@@ -21,134 +24,125 @@ Input: "[4, 5, 6, 5]", "[1, 1, 2, 2]", "[5, 4, 2, 9]"
 Output: "4,5,6,5,2,9,2,4,5,1,1,2"
 */
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <sstream>
-#include <cctype>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-string trim(const string& str)
-{
-  size_t begin_str{};
-  size_t end_str{str.size() - 1};
+string trim(const string& input) {
+  string output{input};
+  output.erase(begin(output),
+               find_if(begin(output), end(output),
+                       [](const char ch) { return !isspace(ch); }));
 
-  if (0u == str.length()) return string{};
+  output.erase(find_if(output.rbegin(), output.rend(),
+                       [](const char ch) { return !isspace(ch); })
+                   .base(),
+               end(output));
 
-  for (; begin_str <= end_str; ++begin_str)
-  {
-    if (!isspace(str[begin_str])) break;
-  }
-
-  if (begin_str > end_str) return string{};
-
-  for (; end_str > begin_str; --end_str)
-  {
-    if (!isspace(str[end_str])) break;
-  }
-
-  return str.substr(begin_str, end_str - begin_str + 1);
+  return output;
 }
 
-vector<string> split(const string& source, const char* needle, size_t const max_count = string::npos)
-{
-  vector<string> parts{}; 
+vector<string> split(const string& source,
+                     const char* needle,
+                     size_t const max_count = string::npos) {
+  vector<string> parts{};
 
   string needle_st{needle};
 
   const size_t source_len{source.length()};
 
-  const size_t needle_len{needle_st.size()};
+  const size_t needle_len{needle_st.length()};
 
-  if ((0u == source_len) || (0u == needle_len)) return parts;
+  if (!source_len)
+    return parts;
+
+  if (!needle_len) {
+    const size_t upper_limit{max_count < source_len ? max_count : source_len};
+    for (size_t i{}; i < upper_limit; i++)
+      parts.emplace_back(1, source[i]);
+    return parts;
+  }
 
   size_t number_of_parts{}, prev{};
 
-  while (true)
-  {
-    const size_t current = source.find(needle_st, prev);
+  while (true) {
+    const size_t current{source.find(needle_st, prev)};
 
-    if (string::npos == current) break;
+    if (string::npos == current)
+      break;
 
     number_of_parts++;
 
-    if ((string::npos != max_count) && (parts.size() == max_count)) break;
+    if ((string::npos != max_count) && (parts.size() == max_count))
+      break;
 
-    if ((current - prev) > 0) parts.emplace_back(source.substr(prev, current - prev));
+    if (current - prev > 0)
+      parts.emplace_back(source.substr(prev, current - prev));
 
     prev = current + needle_len;
 
-    if (prev >= source_len) break;
-  } 
+    if (prev >= source_len)
+      break;
+  }
 
-  if (prev < source_len)
-  {
-    if (string::npos == max_count) parts.emplace_back(source.substr(prev));
+  if (prev < source_len) {
+    if (string::npos == max_count)
+      parts.emplace_back(source.substr(prev));
 
-    else if ((string::npos != max_count) && (parts.size() < max_count)) parts.emplace_back(source.substr(prev));
+    else if (parts.size() < max_count)
+      parts.emplace_back(source.substr(prev));
   }
 
   return parts;
 }
 
-string MatrixSpiral(string* str_arr, const size_t str_arr_size) { 
+string MatrixSpiral(string* str_arr, const size_t str_arr_size) {
+  if (!str_arr_size || !str_arr)
+    return string{};
 
-	if (!str_arr_size || !str_arr) return string{};
+  ostringstream oss{};
 
-	 ostringstream oss{};
+  if (1 == str_arr_size) {
+    *str_arr = trim(*str_arr);
+    str_arr->erase(begin(*str_arr));
+    str_arr->erase(--end(*str_arr));
 
-	if (1u == str_arr_size) {
+    vector<string> elements_str{split(*str_arr, ", ")};
 
-		*str_arr = trim(*str_arr);
+    for (size_t i{}; i < elements_str.size() - 1; i++)
+      oss << elements_str[i] << ',';
 
-		str_arr->erase(begin(*str_arr));
-		str_arr->erase(--end(*str_arr));
+    oss << elements_str[elements_str.size() - 1];
 
-		vector<string> elements_str { split(*str_arr, ", ") };
+    return oss.str();
+  }
 
-		for (size_t i{}; i < elements_str.size() - 1; i++) {
-			elements_str[i] = trim(elements_str[i]);
-			oss << elements_str[i] << ',';
-		}
-
-		elements_str[elements_str.size() - 1] = trim(elements_str[elements_str.size() - 1]);
-		oss << elements_str[elements_str.size() - 1];
-		return oss.str();
-	}
-
-  vector<vector<string>> matrix(str_arr_size, vector<string>{});
+  vector<vector<int>> matrix(str_arr_size, vector<int>{});
 
   size_t prev_column_dim{string::npos};
 
   for (size_t i{}; i < str_arr_size; i++) {
+    str_arr[i] = trim(str_arr[i]);
+    str_arr[i].erase(begin(str_arr[i]));
+    str_arr[i].erase(--end(str_arr[i]));
 
-  	str_arr[i] = trim(str_arr[i]);
-  	str_arr[i].erase(begin(str_arr[i]));
-	  str_arr[i].erase(--end(str_arr[i]));
-	  
-    vector<string> elements_str { split(str_arr[i], ", ") };
+    vector<string> elements_str{split(str_arr[i], ", ")};
 
     // we check if the separate rows of the matix have differing column sizes
-    if (string::npos == prev_column_dim) {
-
+    if (string::npos == prev_column_dim)
       prev_column_dim = elements_str.size();
+    else if (elements_str.size() != prev_column_dim)
+      return "not possible";
 
-    } else if (elements_str.size() != prev_column_dim) {
-
-      return string{"Not possible!"};
-
-    }
-	  
     matrix[i].resize(elements_str.size());
-	
-    for (size_t j{}; j < elements_str.size(); j++) { 
-		  
-      elements_str[j] = trim(elements_str[j]);
-		  matrix[i][j] = elements_str[j];
 
-	  }
-
+    for (size_t j{}; j < elements_str.size(); j++)
+      matrix[i][j] = stoi(elements_str[j]);
   }
 
   int min_x{}, max_x{static_cast<int>(str_arr_size - 1)};
@@ -156,82 +150,86 @@ string MatrixSpiral(string* str_arr, const size_t str_arr_size) {
 
   const size_t max_cells{str_arr_size * matrix[0].size()};
 
-  // starting point (min_x, min_y)
-  int x{min_x}, y{min_y};
+  // starting point is the upper-left corner of 'matrix' (min_x, min_y)
+  int x{}, y{};
 
   size_t cells_visited{};
 
   while (true) {
+    while (y <= max_y && cells_visited < max_cells) {
+      oss << matrix[x][y] << ',';
+      cells_visited++;
+      y++;
+    }
 
-  	while ((y <= max_y) && (cells_visited < max_cells)) {  		
-  		oss << matrix[x][y] << ',';
-  		cells_visited++;
-  		y++;
-  	}
+    if (cells_visited >= max_cells)
+      break;
 
-  	if (cells_visited >= max_cells) break;
+    min_x++;  // we increase min_x by 1 so that we don't accidentally wind up
+              // visiting the previously visited top row again
+    x = min_x;
+    y = max_y;
 
-  	min_x++;
-  	x = min_x;
-  	y = max_y;
+    while (x <= max_x && cells_visited < max_cells) {
+      oss << matrix[x][y] << ',';
+      cells_visited++;
+      x++;
+    }
 
-  	while ((x <= max_x) && (cells_visited < max_cells)) {
+    if (cells_visited >= max_cells)
+      break;
 
-  		oss << matrix[x][y] << ',';
-  		cells_visited++;
-  		x++;
-  	}
+    max_y--;  // we decrease max_y by 1 so that we don't accidentally wind up
+              // visiting the previously visited right column again
+    y = max_y;
+    x = max_x;
 
-  	if (cells_visited >= max_cells) break;
+    while (y >= min_y && cells_visited < max_cells) {
+      oss << matrix[x][y] << ',';
+      cells_visited++;
+      y--;
+    }
 
-  	max_y--; 	
-  	y = max_y;
-  	x = max_x;
-  	
-  	while ((y >= min_y) && (cells_visited < max_cells)) {  		
-  		oss << matrix[x][y] << ',';
-  		cells_visited++;
-  		y--;
-  	}
+    if (cells_visited >= max_cells)
+      break;
 
-  	if (cells_visited >= max_cells) break;
+    max_x--;  // we decrease max_x by 1 so that we don't accidentally wind up
+              // visiting the previously visited bottom row again
+    x = max_x;
+    y = min_y;
 
-  	max_x--;
-  	x = max_x;
-  	y = min_y;
+    while (x >= min_x && cells_visited < max_cells) {
+      oss << matrix[x][y] << ',';
+      cells_visited++;
+      x--;
+    }
 
-  	while ((x >= min_x) && (cells_visited < max_cells)) {
-  		oss << matrix[x][y] << ',';
-  		cells_visited++;
-  		x--;
-  	}
+    if (cells_visited >= max_cells)
+      break;
 
-  	if (cells_visited >= max_cells) break;
-
-  	min_y++;
-  	y = min_y;
-  	x = min_x;
-
+    min_y++;  // we increase min_y by 1 so that we don't accidentally wind up
+              // visiting the previously visited left column again
+    y = min_y;
+    x = min_x;
   }
 
   string result{oss.str()};
-
-  result.erase(--end(result));
-
-  return result; 
-            
+  result.erase(result.length() - 1, 1);
+  return result;
 }
 
-int main() { 
-   
+int main() {
   // string A[] = gets(stdin);
   // cout << MatrixSpiral(A, sizeof(A)/sizeof(*A));
   string B[] = {"[1, 2, 3]", "[4, 5, 6]", "[7, 8, 9]"};
-  cout << MatrixSpiral(B, sizeof(B)/sizeof(*B)) << '\n';  // expected output: "1,2,3,6,9,8,7,4,5"
+  cout << MatrixSpiral(B, sizeof(B) / sizeof(*B))
+       << '\n';  // expected output: "1,2,3,6,9,8,7,4,5"
   string C[] = {"[1, 2]", "[10, 14]"};
-  cout << MatrixSpiral(C, sizeof(C)/sizeof(*C)) << '\n';  // expected output: "1,2,14,10"
+  cout << MatrixSpiral(C, sizeof(C) / sizeof(*C))
+       << '\n';  // expected output: "1,2,14,10"
   string D[] = {"[4, 5, 6, 5]", "[1, 1, 2, 2]", "[5, 4, 2, 9]"};
-  cout << MatrixSpiral(D, sizeof(D)/sizeof(*D)) << '\n';  // expected output: "4,5,6,5,2,9,2,4,5,1,1,2"
-  
-  return 0;    
+  cout << MatrixSpiral(D, sizeof(D) / sizeof(*D))
+       << '\n';  // expected output: "4,5,6,5,2,9,2,4,5,1,1,2"
+
+  return 0;
 }
