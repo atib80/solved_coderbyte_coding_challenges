69bd4ea8a228cb5d958d16908a2a107175df1788
Atila Budai <atib1980@gmail.com>
2018-03-25 09:54:57 +0200

    added TernaryConverter_v2(int) fn

--- 2_medium_difficulty_level/60_ternary_converter.cpp
+++ 2_medium_difficulty_level/60_ternary_converter.cpp
@@ -1,8 +1,10 @@
 /*
 Coderbyte coding challenge: Ternary Converter
 
-Using the C++ language, have the function TernaryConverter(num) take the num parameter being passed, which will always be a positive integer, 
-and convert it into a ternary representation. For example: if num is 12 then your program should return 110.
+Using the C++ language, have the function TernaryConverter(num) take the num
+parameter being passed, which will always be a positive integer, and convert it
+into a ternary representation. For example: if num is 12 then your program
+should return 110.
 
 Sample test cases:
 
@@ -14,38 +16,44 @@ Output: 2111
 */
 
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
 
-string TernaryConverter(int num) { 
-
+string TernaryConverter_v1(int num) {
   string result{};
 
-  result.reserve(16); 
-
   while (num) {
+    result.insert(begin(result), static_cast<char>(num % 3 + '0'));
 
-  	const int factor { num % 3};
-
-  	result.insert(0, 1, static_cast<char>('0' + factor));
-
-  	num /= 3;  	
-
+    num /= 3;
   }
 
-  result.shrink_to_fit();
-
   return result;
-  
 }
 
-int main() { 
-  
-  // cout << TernaryConverter(gets(stdin));
-  cout << TernaryConverter(12) << '\n'; // expected output: 110
-  cout << TernaryConverter(21) << '\n'; // expected output: 210
-  cout << TernaryConverter(67) << '\n'; // expected output: 2111
+string TernaryConverter_v2(int num) {
+  ostringstream oss{};
 
-  return 0;    
+  while (num) {
+    oss << static_cast<char>(num % 3 + '0');
+
+    num /= 3;
+  }
+
+  string result{oss.str()};
+
+  reverse(begin(result), end(result));
+
+  return result;
+}
+
+int main() {
+  // cout << TernaryConverter_v1(gets(stdin));
+  cout << TernaryConverter_v1(12) << '\n';  // expected output: 110
+  cout << TernaryConverter_v1(21) << '\n';  // expected output: 210
+  cout << TernaryConverter_v1(67) << '\n';  // expected output: 2111
+
+  return 0;
 }
