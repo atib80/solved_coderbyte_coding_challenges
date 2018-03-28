e97c07f6c410cf30b8b6f990d30c4885120b168c
Atila Budai <atib1980@gmail.com>
2018-03-28 13:02:44 +0200

    updated, formatted program code

--- 2_medium_difficulty_level/67_simple_password.cpp
+++ 2_medium_difficulty_level/67_simple_password.cpp
@@ -1,8 +1,9 @@
 /*
 Coderbyte coding challenge: Simple Password
 
-Using the C++ language, have the function SimplePassword(str) take the str parameter being passed and determine if it passes as a valid password that follows 
-the list of constraints:
+Using the C++ language, have the function SimplePassword(str) take the str
+parameter being passed and determine if it passes as a valid password that
+follows the list of constraints:
 
 1. It must have a capital letter.
 2. It must contain at least one number.
@@ -10,7 +11,8 @@ the list of constraints:
 4. It cannot have the word "password" in the string.
 5. It must be longer than 7 characters and shorter than 31 characters.
 
-If all the above constraints are met within the string, the your program should return the string true, otherwise your program should return the string false. 
+If all the above constraints are met within the string, then your program should
+return the string true, otherwise your program should return the string false.
 For example: if str is "apple!M7" then your program should return "true".
 
 Sample test cases:
@@ -22,79 +24,96 @@ Input:  "turkey90AAA="
 Output: "true"
 */
 
-#include <iostream>
-#include <cctype>
-#include <string>
 #include <algorithm>
+#include <cctype>
+#include <iostream>
 #include <locale>
+#include <string>
 
 using namespace std;
 
-string trim(const string& str)
-{
-  size_t begin_str{};
-  size_t end_str{str.size() - 1};
+template <typename CharType>
+bool is_ws_char(CharType ch) {
+  switch (ch) {
+    case static_cast<CharType>(' '):
+    case static_cast<CharType>('\t'):
+    case static_cast<CharType>('\n'):
+    case static_cast<CharType>('\r'):
+    case static_cast<CharType>('\f'):
+    case static_cast<CharType>('\v'):
+      return true;
 
-  if (0u == str.length()) return string{};
-
-  for (; begin_str <= end_str; ++begin_str)
-  {
-    if (!isspace(str[begin_str])) break;
+    default:
+      return false;
   }
-
-  if (begin_str > end_str) return string{};
-
-  for (; end_str > begin_str; --end_str)
-  {
-    if (!isspace(str[end_str])) break;
-  }
-
-  return str.substr(begin_str, end_str - begin_str + 1);
 }
 
-size_t strstri(const string& src, const string& needle, const std::locale& loc = std::locale{})
-{
+template <typename StringType>
+StringType trim(const StringType& input) {
+  using char_type = typename StringType::value_type;
+  StringType output{input};
+  output.erase(begin(output),
+               find_if(begin(output), end(output),
+                       [](const char_type ch) { return !is_ws_char(ch); }));
 
-	string src_lc{ src };
-	string needle_lc{ needle };
+  output.erase(find_if(output.rbegin(), output.rend(),
+                       [](const char_type ch) { return !is_ws_char(ch); })
+                   .base(),
+               end(output));
 
-	transform(begin(src), end(src), begin(src_lc), [&](const char ch) {
-		return tolower(ch, loc);
-	});
-
-	transform(begin(needle), end(needle), begin(needle_lc), [&](const char ch) {
-		return tolower(ch, loc);
-	});
-	
-	return src_lc.find(needle_lc);
+  return output;
 }
 
-string SimplePassword(string str) { 
+template <typename StringType>
+size_t strstri(const StringType& src,
+               typename StringType::const_pointer needle,
+               const std::locale& loc = std::locale{}) {
+  using char_type = typename StringType::value_type;
+  StringType src_lc{src};
+  StringType needle_str{needle};
+  StringType needle_lc{needle};
 
+  transform(begin(src), end(src), begin(src_lc),
+            [&](const char_type ch) { return tolower(ch, loc); });
+
+  transform(begin(needle_str), end(needle_str), begin(needle_lc),
+            [&](const char_type ch) { return tolower(ch, loc); });
+
+  return src_lc.find(needle_lc);
+}
+
+string SimplePassword(string str) {
   str = trim(str);
-  const size_t str_len { str.length() };
-  
-  if (str_len < 7 || str_len > 31) return string{"false"}; 
-  
-  if (strstri(str, "password") != string::npos) return string{"false"}; 
-  
-  if (!any_of(begin(str), end(str), [](const char ch) { return isupper(ch); })) return string{"false"};
+  const size_t str_len{str.length()};
 
-  if (!any_of(begin(str), end(str), [](const char ch) { return isdigit(ch); })) return string{"false"};
+  if (str_len < 7 || str_len > 31)
+    return "false";
 
-  if (!any_of(begin(str), end(str), [](const char ch) { return ispunct(ch); })) return string{"false"};
+  if (string::npos != strstri(str, "password"))
+    return "false";
 
-  return string{"true"};            
+  if (!any_of(begin(str), end(str), [](const char ch) { return isupper(ch); }))
+    return "false";
+
+  if (!any_of(begin(str), end(str), [](const char ch) { return isdigit(ch); }))
+    return "false";
+
+  if (!any_of(begin(str), end(str), [](const char ch) { return ispunct(ch); }))
+    return "false";
+
+  return "true";
 }
 
-int main() { 
-  
+int main() {
   // cout << SimplePassword(move(string{gets(stdin)}));
-  cout << SimplePassword(move(string{"apple!M7"})) << '\n';                  // expected output: "true"
-  cout << SimplePassword(move(string{"passWord123!!!!"})) << '\n';           // expected output: "false"
-  cout << SimplePassword(move(string{"turkey90AAA="})) << '\n';              // expected output: "true"
-  cout << SimplePassword(move(string{"!!!!!!+++++++++AMAMAM9"})) << '\n';    // expected output: "true"
+  cout << SimplePassword(move(string{"apple!M7"}))
+       << '\n';  // expected output: "true"
+  cout << SimplePassword(move(string{"passWord123!!!!"}))
+       << '\n';  // expected output: "false"
+  cout << SimplePassword(move(string{"turkey90AAA="}))
+       << '\n';  // expected output: "true"
+  cout << SimplePassword(move(string{"!!!!!!+++++++++AMAMAM9"}))
+       << '\n';  // expected output: "true"
 
   return 0;
-
-} 
+}
