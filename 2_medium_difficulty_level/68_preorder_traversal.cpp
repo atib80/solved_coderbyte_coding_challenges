b535e720305cde15ab2b1fc272216be88aff8fdc
Atila Budai <atib1980@gmail.com>
2018-03-29 13:03:35 +0200

    converted recursive sol. to iterative using queue and stack

--- 2_medium_difficulty_level/68_preorder_traversal.cpp
+++ 2_medium_difficulty_level/68_preorder_traversal.cpp
@@ -1,13 +1,17 @@
 /*
 Coderbyte coding challenge: Preorder Traversal
+(iterative solution implemented by using STL queue and stack)
 
-Using the C++ language, have the function PreorderTraversal(strArr) take the array of strings stored in strArr, which will represent a binary tree with integer values 
-in a format similar to how a binary heap is implemented with NULL nodes at any level represented with a #. 
-Your goal is to return the pre-order traversal of the tree with the elements separated by a space. 
-For example: if strArr is ["5", "2", "6", "1", "9", "#", "8", "#", "#", "#", "#", "4", "#"] then this tree looks like the following tree:
+Using the C++ language, have the function PreorderTraversal(strArr) take the
+array of strings stored in strArr, which will represent a binary tree with
+integer values in a format similar to how a binary heap is implemented with NULL
+nodes at any level represented with a #. Your goal is to return the pre-order
+traversal of the tree with the elements separated by a space. For example: if
+strArr is ["5", "2", "6", "1", "9", "#", "8", "#", "#", "#", "#", "4", "#"] then
+this tree looks like the following tree:
 
-For the input above, your program should return the string 5 2 1 9 6 8 4 because that is the pre-order traversal of the tree.
-Sample Test Cases
+For the input above, your program should return the string 5 2 1 9 6 8 4 because
+that is the pre-order traversal of the tree. Sample Test Cases
 
 Input:  {"4", "1", "5", "2", "#", "#", "#"}
 Output: "4 1 2 5"
@@ -16,83 +20,93 @@ Input:  {"2", "6", "#"}
 Output: "2 6"
 */
 
-#include <iostream>
-#include <sstream>
-#include <string>
+#include <algorithm>
 #include <cctype>
+#include <cmath>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-string trim(const string& str)
-{
-  size_t begin_str{};
-  size_t end_str{str.size() - 1};
+string trim(const string& input) {
+  string output{input};
+  output.erase(begin(output),
+               find_if(begin(output), end(output),
+                       [](const char ch) { return !isspace(ch); }));
 
-  if (0u == str.length()) return string{};
+  output.erase(find_if(output.rbegin(), output.rend(),
+                       [](const char ch) { return !isspace(ch); })
+                   .base(),
+               end(output));
 
-  for (; begin_str <= end_str; ++begin_str)
-  {
-    if (!isspace(str[begin_str])) break;
-  }
-
-  if (begin_str > end_str) return string{};
-
-  for (; end_str > begin_str; --end_str)
-  {
-    if (!isspace(str[end_str])) break;
-  }
-
-  return str.substr(begin_str, end_str - begin_str + 1);
+  return output;
 }
 
-void list_out_contents_using_preorder_traversal(const vector<string>& binary_tree, size_t i, ostringstream& res) {
+string print_out_btree_contents_using_preorder_traversal(
+    const vector<string>& binary_tree) {
+  queue<size_t> q{{0}};
+  stack<size_t> s{};
+  ostringstream oss{};
 
-	if (i >= binary_tree.size()) return;
+  while (!q.empty()) {
+    const size_t index{q.front()};
+    q.pop();
 
-	if (binary_tree[i] == "#") return;	
+    if (index >= binary_tree.size() || "#" == binary_tree[index]) {
+      if (!s.empty()) {
+        q.emplace(s.top());
+        s.pop();
+      }
+      continue;
+    }
 
-	res << binary_tree[i] << ' ';
+    oss << binary_tree[index] << ' ';
 
-	if (((2 * i + 1) >= binary_tree.size()) && (binary_tree[i - 1] == "#")) i--;	
-	
-	list_out_contents_using_preorder_traversal(binary_tree, 2 * i + 1, res);
+    q.emplace(2 * index + 1);
+    s.emplace(2 * index + 2);
+  }
 
-	list_out_contents_using_preorder_traversal(binary_tree, 2 * i + 2, res);
+  string output{oss.str()};
+  output.erase(--end(output));
 
+  return output;
 }
 
 string PreorderTraversal(string* str_arr, const size_t str_arr_size) {
-
   vector<string> items(str_arr, str_arr + str_arr_size);
 
-  for (auto& i : items) i = trim(i);
+  if (static_cast<size_t>(log2(str_arr_size + 1)) !=
+      static_cast<size_t>(log2(str_arr_size)) + 1) {
+    for (size_t i{}; i < (1u << static_cast<size_t>(log2(str_arr_size))) - 1;
+         i++) {
+      if ("#" == items[i])
+        items.insert(begin(items) + 2 * i + 1, {"#", "#"});
+    }
+  }
 
-  ostringstream ross{};
-
-  list_out_contents_using_preorder_traversal(items, 0u, ross);
-
-  string result { ross.str() }; 
-
-  result.erase(--end(result));        
-
-  return result;
+  return print_out_btree_contents_using_preorder_traversal(items);
 }
 
-int main() { 
-   
-    
+int main() {
   // string A[] = gets(stdin);
   // cout << PreorderTraversal(A, sizeof(A)/sizeof(*A));
-  string B[] = {"5", "2", "6", "1", "9", "#", "8", "#", "#", "#", "#", "4", "#"};
-  cout << '"' << PreorderTraversal(B, sizeof(B)/sizeof(*B)) << '"' << '\n'; // expected output: "5 2 1 9 6 8 4"
+  string B[] = {"5", "2", "6", "1", "9", "#", "8",
+                "#", "#", "#", "#", "4", "#"};
+  cout << '"' << PreorderTraversal(B, sizeof(B) / sizeof(*B)) << '"'
+       << '\n';  // expected output: "5 2 1 9 6 8 4"
   string C[] = {"4", "1", "5", "2", "#", "#", "#"};
-  cout << '"' << PreorderTraversal(C, sizeof(C)/sizeof(*C)) << '"' << '\n'; // expected output: "4 1 2 5"
+  cout << '"' << PreorderTraversal(C, sizeof(C) / sizeof(*C)) << '"'
+       << '\n';  // expected output: "4 1 2 5"
   string D[] = {"2", "6", "#"};
-  cout << '"' << PreorderTraversal(D, sizeof(D)/sizeof(*D)) << '"' << '\n'; // expected output: "2 6"
+  cout << '"' << PreorderTraversal(D, sizeof(D) / sizeof(*D)) << '"'
+       << '\n';  // expected output: "2 6"
   string E[] = {"5"};
-  cout << '"' << PreorderTraversal(E, sizeof(E)/sizeof(*E)) << '"' << '\n'; // expected output: "5"
+  cout << '"' << PreorderTraversal(E, sizeof(E) / sizeof(*E)) << '"'
+       << '\n';  // expected output: "5"
 
   return 0;
-
 }
