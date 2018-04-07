caf20f464686f35e1a6304b2e76273f4d72bc9d0
Atila Budai <atib1980@gmail.com>
2018-04-07 19:43:59 +0200

    small update

--- 3_hard_difficulty_level/04_weighted_path_v3.cpp
+++ 3_hard_difficulty_level/04_weighted_path_v3.cpp
@@ -39,8 +39,8 @@ Output: "-1"
 
 #include <algorithm>
 #include <cctype>
-#include <limits>
 #include <iostream>
+#include <limits>
 #include <sstream>
 #include <string>
 #include <unordered_map>
@@ -49,8 +49,7 @@ Output: "-1"
 
 using namespace std;
 
-string trim(const string& input)
-{
+string trim(const string& input) {
   string output{input};
   output.erase(begin(output),
                find_if(begin(output), end(output),
@@ -58,7 +57,7 @@ string trim(const string& input)
 
   output.erase(find_if(output.rbegin(), output.rend(),
                        [](const char ch) { return !isspace(ch); })
-               .base(),
+                   .base(),
                end(output));
 
   return output;
@@ -66,8 +65,7 @@ string trim(const string& input)
 
 vector<string> split(const string& source,
                      const char* needle,
-                     size_t const max_count = string::npos)
-{
+                     size_t const max_count = string::npos) {
   vector<string> parts{};
 
   string needle_st{needle};
@@ -79,8 +77,7 @@ vector<string> split(const string& source,
   if (!source_len)
     return parts;
 
-  if (!needle_len)
-  {
+  if (!needle_len) {
     const size_t upper_limit{max_count < source_len ? max_count : source_len};
     for (size_t i{}; i < upper_limit; i++)
       parts.emplace_back(1, source[i]);
@@ -89,8 +86,7 @@ vector<string> split(const string& source,
 
   size_t number_of_parts{}, prev{};
 
-  while (true)
-  {
+  while (true) {
     const size_t current{source.find(needle_st, prev)};
 
     if (string::npos == current)
@@ -110,8 +106,7 @@ vector<string> split(const string& source,
       break;
   }
 
-  if (prev < source_len)
-  {
+  if (prev < source_len) {
     if (string::npos == max_count)
       parts.emplace_back(source.substr(prev));
 
@@ -122,8 +117,7 @@ vector<string> split(const string& source,
   return parts;
 }
 
-string weighted_path_v3(string* str_arr, const size_t str_arr_size)
-{
+string weighted_path_v3(string* str_arr, const size_t str_arr_size) {
   const size_t N{stoul(str_arr[0])};
 
   string vertices(N, '*');
@@ -133,22 +127,16 @@ string weighted_path_v3(string* str_arr, const size_t str_arr_size)
   for (size_t i{1}; i <= N; i++)
     vertices[i - 1] = str_arr[i][0];
 
-  for (size_t i{N + 1}; i < str_arr_size; i++)
-  {
+  for (size_t i{N + 1}; i < str_arr_size; i++) {
     str_arr[i] = trim(str_arr[i]);
-
-    const char v1{str_arr[i][0]};
-
-    const char v2{str_arr[i][2]};
 
     const int w{stoi(str_arr[i].substr(4))};
 
-    string key{{v1, v2}};
+    string key{{str_arr[i][0], str_arr[i][2]}};
 
     auto itr = graph_edge_weight.find(key);
 
-    if (itr != end(graph_edge_weight))
-    {
+    if (itr != end(graph_edge_weight)) {
       if (w < itr->second)
         itr->second = w;
 
@@ -159,9 +147,7 @@ string weighted_path_v3(string* str_arr, const size_t str_arr_size)
       if (itr != end(graph_edge_weight))
         if (w < itr->second)
           itr->second = w;
-    }
-    else
-    {
+    } else {
       graph_edge_weight[key] = w;
       swap(key[0], key[1]);
       graph_edge_weight[key] = w;
@@ -180,8 +166,7 @@ string weighted_path_v3(string* str_arr, const size_t str_arr_size)
 
   sort(begin(vertices), end(vertices));
 
-  do
-  {
+  do {
     string current_edge{{source_vertex, vertices.front()}};
 
     if (graph_edge_weight.find(current_edge) == end(graph_edge_weight))
@@ -193,10 +178,8 @@ string weighted_path_v3(string* str_arr, const size_t str_arr_size)
 
     int current_weight{graph_edge_weight[current_edge]};
 
-    if (destination_vertex == vertices.front())
-    {
-      if (current_weight < min_distance)
-      {
+    if (destination_vertex == vertices.front()) {
+      if (current_weight < min_distance) {
         min_distance = current_weight;
 
         current_path = oss.str();
@@ -209,18 +192,15 @@ string weighted_path_v3(string* str_arr, const size_t str_arr_size)
 
     bool is_path_connected{true};
 
-    for (size_t i{}; i < vertices.length() - 1; i++)
-    {
+    for (size_t i{}; i < vertices.length() - 1; i++) {
       current_edge.assign({vertices[i], vertices[i + 1]});
 
-      if (visited_nodes.find(vertices[i + 1]) != end(visited_nodes))
-      {
+      if (visited_nodes.find(vertices[i + 1]) != end(visited_nodes)) {
         is_path_connected = false;
         break;
       }
 
-      if (graph_edge_weight.find(current_edge) == end(graph_edge_weight))
-      {
+      if (graph_edge_weight.find(current_edge) == end(graph_edge_weight)) {
         is_path_connected = false;
         break;
       }
@@ -229,8 +209,7 @@ string weighted_path_v3(string* str_arr, const size_t str_arr_size)
 
       current_weight += graph_edge_weight[current_edge];
 
-      if (current_weight >= min_distance)
-      {
+      if (current_weight >= min_distance) {
         is_path_connected = false;
         break;
       }
@@ -241,55 +220,64 @@ string weighted_path_v3(string* str_arr, const size_t str_arr_size)
         break;
     }
 
-    if (is_path_connected && current_weight < min_distance)
-    {
+    if (is_path_connected && current_weight < min_distance) {
       min_distance = current_weight;
 
       current_path = oss.str();
     }
-  }
-  while (next_permutation(begin(vertices), end(vertices)));
+  } while (next_permutation(begin(vertices), end(vertices)));
 
   return current_path;
 }
 
-int main()
-{
+int main() {
   // string A[] = gets(stdin);
-  // cout << weighted_path_v3(A, sizeof(A), sizeof(*A));  
+  // cout << weighted_path_v3(A, sizeof(A), sizeof(*A));
 
   string B[] = {"4", "A", "B", "C", "D", "A|B|1", "B|D|9", "B|C|3", "C|D|4"};
-  cout << weighted_path_v3(B, sizeof(B) / sizeof(*B)) << '\n'; // expected output: "A-B-C-D"
+  cout << weighted_path_v3(B, sizeof(B) / sizeof(*B))
+       << '\n';  // expected output: "A-B-C-D"
 
-  string C[] = {"7", "A", "B", "C", "D", "E", "F", "G", "A|B|1", "A|E|9", "B|C|2", "C|D|1", "D|F|2", "E|D|6", "F|G|2"};
-  cout << weighted_path_v3(C, sizeof(C) / sizeof(*C)) << '\n'; // expected output: "A-B-C-D-F-G"
+  string C[] = {"7",     "A",     "B",     "C",     "D",
+                "E",     "F",     "G",     "A|B|1", "A|E|9",
+                "B|C|2", "C|D|1", "D|F|2", "E|D|6", "F|G|2"};
+  cout << weighted_path_v3(C, sizeof(C) / sizeof(*C))
+       << '\n';  // expected output: "A-B-C-D-F-G"
 
   string D[] = {"4", "A", "B", "C", "D", "A|B|2", "C|B|11", "C|D|3", "B|D|2"};
-  cout << weighted_path_v3(D, sizeof(D) / sizeof(*D)) << '\n'; // expected output: "A-B-D"
+  cout << weighted_path_v3(D, sizeof(D) / sizeof(*D))
+       << '\n';  // expected output: "A-B-D"
 
   string E[] = {"4", "x", "y", "z", "w", "x|y|2", "y|z|14", "z|y|31"};
-  cout << weighted_path_v3(E, sizeof(E) / sizeof(*E)) << '\n'; // expected output: "-1"
+  cout << weighted_path_v3(E, sizeof(E) / sizeof(*E))
+       << '\n';  // expected output: "-1"
 
-  string F[] = {"6", "A", "B", "C", "D", "E", "F", "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|1", "C|D|4", "F|E|1"};
-  cout << weighted_path_v3(F, sizeof(F) / sizeof(*F)) << '\n'; // expected output: "A-F"
+  string F[] = {"6",     "A",     "B",     "C",     "D",     "E",     "F",
+                "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|1", "C|D|4", "F|E|1"};
+  cout << weighted_path_v3(F, sizeof(F) / sizeof(*F))
+       << '\n';  // expected output: "A-F"
 
-  string G[] = {"6", "D", "B", "C", "A", "E", "F", "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|12", "C|D|4", "F|E|1"};
-  cout << weighted_path_v3(G, sizeof(G) / sizeof(*G)) << '\n'; // expected output: "D-B-A-F"
+  string G[] = {"6",     "D",     "B",     "C",     "A",      "E",     "F",
+                "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|12", "C|D|4", "F|E|1"};
+  cout << weighted_path_v3(G, sizeof(G) / sizeof(*G))
+       << '\n';  // expected output: "D-B-A-F"
 
-  string H[] = {
-    "8", "C", "B", "A", "D", "E", "F", "G", "H", "C|D|1", "D|F|2", "G|F|2", "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2",
-    "C|E|9"
-  };
-  cout << weighted_path_v3(H, sizeof(H) / sizeof(*H)) << '\n'; // expected output: "C-D-F-G-E-B-H"
+  string H[] = {"8",     "C",     "B",     "A",      "D",     "E",
+                "F",     "G",     "H",     "C|D|1",  "D|F|2", "G|F|2",
+                "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2", "C|E|9"};
+  cout << weighted_path_v3(H, sizeof(H) / sizeof(*H))
+       << '\n';  // expected output: "C-D-F-G-E-B-H"
 
-  string I[] = {"5", "c", "a", "b", "d", "e", "c|a|3", "a|b|2", "a|d|34", "b|e|15", "e|d|2"};
-  cout << weighted_path_v3(I, sizeof(I) / sizeof(*I)) << '\n'; // expected output: "c-a-b-e"
+  string I[] = {"5",     "c",     "a",      "b",      "d",    "e",
+                "c|a|3", "a|b|2", "a|d|34", "b|e|15", "e|d|2"};
+  cout << weighted_path_v3(I, sizeof(I) / sizeof(*I))
+       << '\n';  // expected output: "c-a-b-e"
 
-  string J[] = {
-    "8", "C", "B", "A", "D", "E", "F", "G", "H", "C|D|1", "D|F|2","G|F|2", "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2",
-    "C|E|1"
-  };
-  cout << weighted_path_v3(J, sizeof(J) / sizeof(*J)) << '\n'; // expected output: "C-E-B-H"
+  string J[] = {"8",     "C",     "B",     "A",      "D",     "E",
+                "F",     "G",     "H",     "C|D|1",  "D|F|2", "G|F|2",
+                "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2", "C|E|1"};
+  cout << weighted_path_v3(J, sizeof(J) / sizeof(*J))
+       << '\n';  // expected output: "C-E-B-H"
 
   return 0;
 }
