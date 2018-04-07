8e3edc2b8f6afb27db1dbaeb38234d742d39dc2c
Atila Budai <atib1980@gmail.com>
2018-04-07 15:14:09 +0200

    couple of minor updates, formatting

--- 3_hard_difficulty_level/04_weighted_path_v2.cpp
+++ 3_hard_difficulty_level/04_weighted_path_v2.cpp
@@ -1,7 +1,7 @@
 /*
 Coderbyte coding challenge: Weighted Path v2
 
-(recursive solution)
+(optimized recursive solution)
 
 Using the C++ language, have the function WeightedPath(strArr) take strArr which
 will be an array of strings which models a non-looping weighted Graph. The
@@ -48,7 +48,8 @@ Output: "-1"
 
 using namespace std;
 
-string trim(const string& input) {
+string trim(const string& input)
+{
   string output{input};
   output.erase(begin(output),
                find_if(begin(output), end(output),
@@ -56,7 +57,7 @@ string trim(const string& input) {
 
   output.erase(find_if(output.rbegin(), output.rend(),
                        [](const char ch) { return !isspace(ch); })
-                   .base(),
+               .base(),
                end(output));
 
   return output;
@@ -64,7 +65,8 @@ string trim(const string& input) {
 
 vector<string> split(const string& source,
                      const char* needle,
-                     size_t const max_count = string::npos) {
+                     size_t const max_count = string::npos)
+{
   vector<string> parts{};
 
   string needle_st{needle};
@@ -76,7 +78,8 @@ vector<string> split(const string& source,
   if (!source_len)
     return parts;
 
-  if (!needle_len) {
+  if (!needle_len)
+  {
     const size_t upper_limit{max_count < source_len ? max_count : source_len};
     for (size_t i{}; i < upper_limit; i++)
       parts.emplace_back(1, source[i]);
@@ -85,7 +88,8 @@ vector<string> split(const string& source,
 
   size_t number_of_parts{}, prev{};
 
-  while (true) {
+  while (true)
+  {
     const size_t current{source.find(needle_st, prev)};
 
     if (string::npos == current)
@@ -105,7 +109,8 @@ vector<string> split(const string& source,
       break;
   }
 
-  if (prev < source_len) {
+  if (prev < source_len)
+  {
     if (string::npos == max_count)
       parts.emplace_back(source.substr(prev));
 
@@ -116,7 +121,8 @@ vector<string> split(const string& source,
   return parts;
 }
 
-struct path_segment {
+struct path_segment
+{
   string current_vertex;
   string travelled_path;
   unordered_set<string> visited_vertices;
@@ -125,79 +131,85 @@ struct path_segment {
   path_segment(const string& cv,
                const string& path,
                const unordered_set<string>& prev_visited_vertices =
-                   unordered_set<string>{},
+                 unordered_set<string>{},
                const size_t weight = 0)
-      : current_vertex{cv},
-        travelled_path{path},
-        visited_vertices{prev_visited_vertices},
-        path_weight{weight} {}
+    : current_vertex{cv},
+      travelled_path{path},
+      visited_vertices{prev_visited_vertices},
+      path_weight{weight}
+  {
+  }
 };
 
 void create_graph_adjacency_list(
-    string* str_arr,
-    const size_t str_arr_size,
-    vector<string>& vertices,
-    unordered_map<string, unordered_map<string, size_t>>&
-        adjacent_vertices_and_their_weights) {
+  string* str_arr,
+  const size_t str_arr_size,
+  vector<string>& vertices,
+  unordered_map<string, unordered_map<string, size_t>>&
+  adjacent_vertices_and_their_weights)
+{
   const size_t n{stoul(str_arr[0])};
 
-  for (size_t i{1}; i <= n; i++) {
+  for (size_t i{1}; i <= n; i++)
+  {
     vertices.emplace_back(str_arr[i]);
     adjacent_vertices_and_their_weights.insert(
-        make_pair(str_arr[i], unordered_map<string, size_t>{}));
+      make_pair(str_arr[i], unordered_map<string, size_t>{}));
   }
 
-  for (size_t i{n + 1}; i < str_arr_size; i++) {
+  for (size_t i{n + 1}; i < str_arr_size; i++)
+  {
     const vector<string> edge_weight{split(str_arr[i], "|")};
     if (adjacent_vertices_and_their_weights.find(edge_weight[0]) ==
-        end(adjacent_vertices_and_their_weights))
+      end(adjacent_vertices_and_their_weights))
       adjacent_vertices_and_their_weights.insert(
-          make_pair(edge_weight[0], unordered_map<string, size_t>{}));
+        make_pair(edge_weight[0], unordered_map<string, size_t>{}));
     if (adjacent_vertices_and_their_weights[edge_weight[0]].find(
-            edge_weight[1]) ==
-        end(adjacent_vertices_and_their_weights[edge_weight[0]]))
+        edge_weight[1]) ==
+      end(adjacent_vertices_and_their_weights[edge_weight[0]]))
       adjacent_vertices_and_their_weights[edge_weight[0]].insert(
-          make_pair(edge_weight[1], stoul(edge_weight[2])));
+        make_pair(edge_weight[1], stoul(edge_weight[2])));
     else
       adjacent_vertices_and_their_weights[edge_weight[0]][edge_weight[1]] =
-          stoul(edge_weight[2]);
+        stoul(edge_weight[2]);
 
     if (adjacent_vertices_and_their_weights.find(edge_weight[1]) ==
-        end(adjacent_vertices_and_their_weights))
+      end(adjacent_vertices_and_their_weights))
       adjacent_vertices_and_their_weights.insert(
-          make_pair(edge_weight[1], unordered_map<string, size_t>{}));
+        make_pair(edge_weight[1], unordered_map<string, size_t>{}));
     if (adjacent_vertices_and_their_weights[edge_weight[1]].find(
-            edge_weight[0]) ==
-        end(adjacent_vertices_and_their_weights[edge_weight[1]]))
+        edge_weight[0]) ==
+      end(adjacent_vertices_and_their_weights[edge_weight[1]]))
       adjacent_vertices_and_their_weights[edge_weight[1]].insert(
-          make_pair(edge_weight[0], stoul(edge_weight[2])));
+        make_pair(edge_weight[0], stoul(edge_weight[2])));
     else
       adjacent_vertices_and_their_weights[edge_weight[1]][edge_weight[0]] =
-          stoul(edge_weight[2]);
+        stoul(edge_weight[2]);
   }
 }
 
-void find_least_costly_route(
-    const unordered_map<string, unordered_map<string, size_t>>&
-        adjacent_vertices_and_their_weights,
-    unordered_set<string>& already_visited_vertices,
-    const string& current_vertex,
-    const string& dest_vertex,
-    string& shortest_path,
-    vector<string>& shortest_path_vertices,
-    size_t& shortest_path_weight,
-    const size_t path_weight = 0) {
+void find_least_costly_route(const unordered_map<string, unordered_map<string, size_t>>&
+                             adjacent_vertices_and_their_weights,
+                             unordered_set<string>& already_visited_vertices,
+                             const string& current_vertex,
+                             const string& dest_vertex,
+                             string& shortest_path,
+                             vector<string>& shortest_path_vertices,
+                             size_t& shortest_path_weight,
+                             const size_t path_weight = 0)
+{
   const auto cv_iter = adjacent_vertices_and_their_weights.find(current_vertex);
   if (cv_iter == end(adjacent_vertices_and_their_weights))
     return;
 
-  for (const auto& neighbor_vertex : cv_iter->second) {
-    if ((already_visited_vertices.find(neighbor_vertex.first) !=
-         end(already_visited_vertices)) ||
-        (path_weight + neighbor_vertex.second >= shortest_path_weight))
+  for (const auto& neighbor_vertex : cv_iter->second)
+  {
+    if (already_visited_vertices.find(neighbor_vertex.first) != end(already_visited_vertices)
+      || path_weight + neighbor_vertex.second >= shortest_path_weight)
       continue;
 
-    if (neighbor_vertex.first == dest_vertex) {
+    if (neighbor_vertex.first == dest_vertex)
+    {
       shortest_path_weight = path_weight + neighbor_vertex.second;
       ostringstream oss{};
       for (const string& v : shortest_path_vertices)
@@ -221,13 +233,13 @@ void find_least_costly_route(
   }
 }
 
-string weighted_path_v2(string* str_arr, const size_t str_arr_size) {
+string weighted_path_v2(string* str_arr, const size_t str_arr_size)
+{
   vector<string> vertices{};
   unordered_map<string, unordered_map<string, size_t>>
-      adjacent_vertices_and_their_weights{};
+    adjacent_vertices_and_their_weights{};
 
-  create_graph_adjacency_list(str_arr, str_arr_size, vertices,
-                              adjacent_vertices_and_their_weights);
+  create_graph_adjacency_list(str_arr, str_arr_size, vertices, adjacent_vertices_and_their_weights);
 
   size_t shortest_path_weight{string::npos};
   string shortest_path{"-1"};
@@ -236,51 +248,64 @@ string weighted_path_v2(string* str_arr, const size_t str_arr_size) {
 
   find_least_costly_route(adjacent_vertices_and_their_weights,
                           already_visited_vertices, vertices.front(),
-                          vertices.back(), shortest_path,
-                          shortest_path_vertices, shortest_path_weight, 0);
+                          vertices.back(), shortest_path, shortest_path_vertices,
+                          shortest_path_weight, 0);
 
   return shortest_path;
 }
 
-int main() {
+int main()
+{
   // string A[] = gets(stdin);
   // cout << weighted_path_v2(A, sizeof(A)/sizeof(*A));
   string B[] = {"4", "A", "B", "C", "D", "A|B|1", "B|D|9", "B|C|3", "C|D|4"};
   cout << weighted_path_v2(B, sizeof(B) / sizeof(*B))
-       << '\n';  // expected output: "A-B-C-D"
-  string C[] = {"7",     "A",     "B",     "C",     "D",
-                "E",     "F",     "G",     "A|B|1", "A|E|9",
-                "B|C|2", "C|D|1", "D|F|2", "E|D|6", "F|G|2"};
+    << '\n'; // expected output: "A-B-C-D"
+  string C[] = {
+    "7", "A", "B", "C", "D",
+    "E", "F", "G", "A|B|1", "A|E|9",
+    "B|C|2", "C|D|1", "D|F|2", "E|D|6", "F|G|2"
+  };
   cout << weighted_path_v2(C, sizeof(C) / sizeof(*C))
-       << '\n';  // expected output: "A-B-C-D-F-G"
+    << '\n'; // expected output: "A-B-C-D-F-G"
   string D[] = {"4", "A", "B", "C", "D", "A|B|2", "C|B|11", "C|D|3", "B|D|2"};
   cout << weighted_path_v2(D, sizeof(D) / sizeof(*D))
-       << '\n';  // expected output: "A-B-D"
+    << '\n'; // expected output: "A-B-D"
   string E[] = {"4", "x", "y", "z", "w", "x|y|2", "y|z|14", "z|y|31"};
   cout << weighted_path_v2(E, sizeof(E) / sizeof(*E))
-       << '\n';  // expected output: "-1"
-  string F[] = {"6",     "A",     "B",     "C",     "D",     "E",     "F",
-                "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|1", "C|D|4", "F|E|1"};
+    << '\n'; // expected output: "-1"
+  string F[] = {
+    "6", "A", "B", "C", "D", "E", "F",
+    "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|1", "C|D|4", "F|E|1"
+  };
   cout << weighted_path_v2(F, sizeof(F) / sizeof(*F))
-       << '\n';  // expected output: "A-F"
-  string G[] = {"6",     "D",     "B",     "C",     "A",      "E",     "F",
-                "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|12", "C|D|4", "F|E|1"};
+    << '\n'; // expected output: "A-F"
+  string G[] = {
+    "6", "D", "B", "C", "A", "E", "F",
+    "B|A|2", "A|F|3", "A|C|4", "B|D|1", "D|E|12", "C|D|4", "F|E|1"
+  };
   cout << weighted_path_v2(G, sizeof(G) / sizeof(*G))
-       << '\n';  // expected output: "D-B-A-F"
-  string H[] = {"8",     "C",     "B",     "A",      "D",     "E",
-                "F",     "G",     "H",     "C|D|1",  "D|F|2", "G|F|2",
-                "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2", "C|E|9"};
+    << '\n'; // expected output: "D-B-A-F"
+  string H[] = {
+    "8", "C", "B", "A", "D", "E",
+    "F", "G", "H", "C|D|1", "D|F|2", "G|F|2",
+    "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2", "C|E|9"
+  };
   cout << weighted_path_v2(H, sizeof(H) / sizeof(*H))
-       << '\n';  // expected output: "C-D-F-G-E-B-H"
-  string I[] = {"5",     "c",     "a",      "b",      "d",    "e",
-                "c|a|3", "a|b|2", "a|d|34", "b|e|15", "e|d|2"};
+    << '\n'; // expected output: "C-D-F-G-E-B-H"
+  string I[] = {
+    "5", "c", "a", "b", "d", "e",
+    "c|a|3", "a|b|2", "a|d|34", "b|e|15", "e|d|2"
+  };
   cout << weighted_path_v2(I, sizeof(I) / sizeof(*I))
-       << '\n';  // expected output: "c-a-b-e"
-  string J[] = {"8",     "C",     "B",     "A",      "D",     "E",
-                "F",     "G",     "H",     "C|D|1",  "D|F|2", "G|F|2",
-                "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2", "C|E|1"};
+    << '\n'; // expected output: "c-a-b-e"
+  string J[] = {
+    "8", "C", "B", "A", "D", "E",
+    "F", "G", "H", "C|D|1", "D|F|2", "G|F|2",
+    "G|E|1", "E|B|1", "H|B|1", "C|A|13", "B|A|2", "C|E|1"
+  };
   cout << weighted_path_v2(J, sizeof(J) / sizeof(*J))
-       << '\n';  // expected output: "C-E-B-H"
+    << '\n'; // expected output: "C-E-B-H"
 
   return 0;
 }
--- 3_hard_difficulty_level/05_intersecting_lines.cpp
+++ 3_hard_difficulty_level/05_intersecting_lines.cpp
@@ -1,15 +1,19 @@
 /*
 Coderbyte coding challenge: Intersecting Lines
 
-Using the C++ language, have the function IntersectingLines(strArr) take strArr which will be an array of 4 coordinates in the form: (x,y). 
-Your program should take these points where the first 2 form a line and the last 2 form a line, and determine whether the lines intersect, 
-and if they do, at what point. 
-For example: if strArr is ["(3,0)","(1,4)","(0,-3)","(2,3)"], then the line created by (3,0) and (1,4) and the line created by (0,-3) (2,3) intersect at (9/5,12/5). 
-Your output should therefore be the 2 points in fraction form in the following format: (9/5,12/5). 
-If there is no denominator for the resulting points, then the output should just be the integers like so: (12,3). 
-If any of the resulting points is negative, add the negative sign to the numerator like so: (-491/63,-491/67). 
-If there is no intersection, your output should return the string "no intersection". 
-The input points and the resulting points can be positive or negative integers.
+Using the C++ language, have the function IntersectingLines(strArr) take strArr
+which will be an array of 4 coordinates in the form: (x,y). Your program should
+take these points where the first 2 form a line and the last 2 form a line, and
+determine whether the lines intersect, and if they do, at what point. For
+example: if strArr is ["(3,0)","(1,4)","(0,-3)","(2,3)"], then the line created
+by (3,0) and (1,4) and the line created by (0,-3) (2,3) intersect at (9/5,12/5).
+Your output should therefore be the 2 points in fraction form in the following
+format: (9/5,12/5). If there is no denominator for the resulting points, then
+the output should just be the integers like so: (12,3). If any of the resulting
+points is negative, add the negative sign to the numerator like so:
+(-491/63,-491/67). If there is no intersection, your output should return the
+string "no intersection". The input points and the resulting points can be
+positive or negative integers.
 
 Sample test cases:
 
@@ -20,194 +24,203 @@ Input:  "(1,2)","(3,4)","(-5,-6)","(-7,-8)"
 Output: "no intersection"
 */
 
-#include <iostream>
-#include <sstream>
+#include <algorithm>
 #include <cctype>
 #include <cmath>
+#include <iostream>
+#include <sstream>
 #include <string>
-#include <vector>
 #include <utility>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-string trim(const string& str)
-{
-  const size_t str_len{ str.length() };
+string trim(const string& input) {
+  string output{input};
+  output.erase(begin(output),
+               find_if(begin(output), end(output),
+                       [](const char ch) { return !isspace(ch); }));
 
-  if (!str_len) return string{};
+  output.erase(find_if(output.rbegin(), output.rend(),
+                       [](const char ch) { return !isspace(ch); })
+                   .base(),
+               end(output));
 
-  size_t first{}, last{ str_len - 1 };
-
-  for (; first <= last; ++first)
-  {
-    if (!isspace(str[first])) break;
-  }
-
-  if (first > last) return string{};
-
-  for (; last > first; --last)
-  {
-    if (!isspace(str[last])) break;
-  }
-
-  return str.substr(first, last - first + 1);
+  return output;
 }
 
-vector<string> split(const string& source, const char* needle, size_t const max_count = string::npos)
-{
-	vector<string> parts{};
+vector<string> split(const string& source,
+                     const char* needle,
+                     size_t const max_count = string::npos) {
+  vector<string> parts{};
 
-	string needle_st{ needle };
+  string needle_st{needle};
 
-	const size_t source_len{ source.length() };
+  const size_t source_len{source.length()};
 
-	const size_t needle_len{ needle_st.size() };
+  const size_t needle_len{needle_st.length()};
 
-	if ((0u == source_len) || (0u == needle_len)) return parts;
+  if (!source_len)
+    return parts;
 
-	size_t number_of_parts{}, prev{};
+  if (!needle_len) {
+    const size_t upper_limit{max_count < source_len ? max_count : source_len};
+    for (size_t i{}; i < upper_limit; i++)
+      parts.emplace_back(1, source[i]);
+    return parts;
+  }
 
-	while (true)
-	{
-		const size_t current{ source.find(needle_st, prev) };
+  size_t number_of_parts{}, prev{};
 
-		if (string::npos == current) break;
+  while (true) {
+    const size_t current{source.find(needle_st, prev)};
 
-		number_of_parts++;
+    if (string::npos == current)
+      break;
 
-		if ((string::npos != max_count) && (parts.size() == max_count)) break;
+    number_of_parts++;
 
-		if ((current - prev) > 0) parts.emplace_back(source.substr(prev, current - prev));
+    if ((string::npos != max_count) && (parts.size() == max_count))
+      break;
 
-		prev = current + needle_len;
+    if (current - prev > 0)
+      parts.emplace_back(source.substr(prev, current - prev));
 
-		if (prev >= source_len) break;
-	}
+    prev = current + needle_len;
 
-	if (prev < source_len)
-	{
-		if (string::npos == max_count) parts.emplace_back(source.substr(prev));
+    if (prev >= source_len)
+      break;
+  }
 
-		else if ((string::npos != max_count) && (parts.size() < max_count)) parts.emplace_back(source.substr(prev));
-	}
+  if (prev < source_len) {
+    if (string::npos == max_count)
+      parts.emplace_back(source.substr(prev));
 
-	return parts;
+    else if (parts.size() < max_count)
+      parts.emplace_back(source.substr(prev));
+  }
+
+  return parts;
 }
 
- // example for str_arr: { "(3,0)","(1,4)","(0,-3)","(2,3)" }, str_arr_size = 4
+// example for str_arr: { "(3,0)","(1,4)","(0,-3)","(2,3)" }, str_arr_size = 4
 
-vector<pair<int, int>> parse_2d_coordinate_pairs(string* str_arr, const size_t str_arr_size) {
+vector<pair<int, int>> parse_2d_coordinate_pairs(string* str_arr,
+                                                 const size_t str_arr_size) {
+  vector<pair<int, int>> coordinates{};
 
-	vector<pair<int, int>> coordinates{};
+  for (size_t i{}; i < str_arr_size; i++) {
+    str_arr[i] = trim(str_arr[i]);
 
-	for (size_t i{}; i < str_arr_size; i++) {
+    str_arr[i].erase(begin(str_arr[i]));
 
-		str_arr[i] = trim(str_arr[i]);
+    str_arr[i].erase(--end(str_arr[i]));
 
-		str_arr[i].erase(begin(str_arr[i]));
+    const vector<string> number_parts{split(str_arr[i], ",")};
 
-		str_arr[i].erase(--end(str_arr[i]));
+    if (2 != number_parts.size())
+      return vector<pair<int, int>>{};
 
-		auto const number_parts = split(str_arr[i], ",");
+    coordinates.emplace_back(
+        make_pair(stoi(number_parts[0]), stoi(number_parts[1])));
+  }
 
-		if (2u != number_parts.size()) return vector<pair<int, int>>{};
-
-		coordinates.emplace_back(make_pair(stoi(number_parts[0]), stoi(number_parts[1])));
-	}
-
-	return coordinates;
+  return coordinates;
 }
 
 pair<int, int> factorize_rational_number(int nominator, int denominator) {
+  const bool is_rational_number_negative{(nominator < 0 && denominator > 0) ||
+                                         (nominator > 0 && denominator < 0)};
 
-	const bool is_rational_number_negative { (((nominator < 0) && (denominator > 0)) || ((nominator > 0) && (denominator < 0))) };
-	
-	nominator = abs(nominator);
+  nominator = abs(nominator);
 
-	denominator = abs(denominator);
+  denominator = abs(denominator);
 
-	const int min_number { min(nominator, denominator) };
+  const int min_number{min(nominator, denominator)};
 
-	for (int i{min_number}; i > 1; i--) {
+  for (int i{min_number}; i > 1; i--) {
+    if (nominator % i == 0 && denominator % i == 0) {
+      nominator /= i;
 
-		if ( ( nominator % i == 0) && (denominator % i == 0 ) ) {
+      denominator /= i;
 
-			nominator /= i;
+      if (is_rational_number_negative)
+        nominator = -nominator;
 
-			denominator /= i;
+      return make_pair(nominator, denominator);
+    }
+  }
 
-			if (is_rational_number_negative) nominator = -nominator;
+  if (is_rational_number_negative)
+    nominator = -nominator;
 
-			return make_pair(nominator, denominator);
-		}
-
-	}
-
-	if (is_rational_number_negative) nominator = -nominator;
-
-	return make_pair(nominator, denominator);
+  return make_pair(nominator, denominator);
 }
 
-string find_intersection_point_coordinates_of_two_lines(const pair<int, int>& A, const pair<int, int>& B, const pair<int, int>& C, const pair<int, int>& D) {
+string find_intersection_point_coordinates_of_two_lines(
+    const pair<int, int>& A,
+    const pair<int, int>& B,
+    const pair<int, int>& C,
+    const pair<int, int>& D) {
+  // Line 'AB' represented as a1x + b1y = c1
 
-    // Line 'AB' represented as a1x + b1y = c1
-    const int a1 { B.second - A.second };
-    const int b1 { A.first - B.first };
-    const int c1 { a1 * A.first + b1 * A.second };
-  
-    // Line 'CD' represented as a2x + b2y = c2
-    const int a2 { D.second - C.second };
-    const int b2 { C.first - D.first };
-    const int c2 { a2 * C.first + b2 * C.second };
-  
-    const int determinant { a1 * b2 - a2 * b1 };
-  
-    if (!determinant) return "no intersection";
+  const int a1{B.second - A.second};
+  const int b1{A.first - B.first};
+  const int c1{a1 * A.first + b1 * A.second};
 
-    ostringstream oss{};
+  // Line 'CD' represented as a2x + b2y = c2
+  const int a2{D.second - C.second};
+  const int b2{C.first - D.first};
+  const int c2{a2 * C.first + b2 * C.second};
 
-    const int x{b2 * c1 - b1 * c2};
+  const int determinant{a1 * b2 - a2 * b1};
 
-    if ((x % determinant) != 0) {
-    	const auto frn = factorize_rational_number(x, determinant);
-    	oss << '(' << frn.first << '/' << frn.second << ',';
-	} else {
-		oss << '(' << (x / determinant) << ',';
-	}
+  if (!determinant)
+    return "no intersection";
 
-	const int y{a1 * c2 - a2 * c1};
+  ostringstream oss{};
 
-	if ((y % determinant) != 0) {
-    	const auto frn = factorize_rational_number(y, determinant);
-    	oss << frn.first << '/' << frn.second << ')';
-	} else {
-		oss << (y / determinant) << ')';
-	}
-    
-    return oss.str();
+  const int x{b2 * c1 - b1 * c2};
+
+  if (x % determinant != 0) {
+    const auto frn = factorize_rational_number(x, determinant);
+    oss << '(' << frn.first << '/' << frn.second << ',';
+  } else
+    oss << '(' << (x / determinant) << ',';
+
+  const int y{a1 * c2 - a2 * c1};
+
+  if (y % determinant != 0) {
+    const auto frn = factorize_rational_number(y, determinant);
+    oss << frn.first << '/' << frn.second << ')';
+  } else
+    oss << (y / determinant) << ')';
+
+  return oss.str();
 }
 
-string IntersectingLines(string *str_arr, const size_t str_arr_size) {
-
+string IntersectingLines(string* str_arr, const size_t str_arr_size) {
   const auto points = parse_2d_coordinate_pairs(str_arr, str_arr_size);
 
-  if (4u != points.size()) return "no intersection";
-  
-  return find_intersection_point_coordinates_of_two_lines(points[0], points[1], points[2], points[3]);
+  if (4 != points.size())
+    return "no intersection";
 
+  return find_intersection_point_coordinates_of_two_lines(points[0], points[1],
+                                                          points[2], points[3]);
 }
 
-int main() { 
-   
-  string A[] = gets(stdin);
-  cout << IntersectingLines(A, sizeof(A)/sizeof(*A));
-  // string b[] = {"(3,0)","(1,4)","(0,-3)","(2,3)"};
-  // cout << IntersectingLines(b, sizeof(b)/sizeof(*b)) << '\n'; // expected output: "(9/5,12/5)"
-  // string c[] = {"(9,-2)","(-2,9)","(3,4)","(10,11)"};
-  // cout << IntersectingLines(c, sizeof(c)/sizeof(*c)) << '\n'; // expected output: "(3,4)"
-  // string d[] = {"(1,2)","(3,4)","(-5,-6)","(-7,-8)"};
-  // cout << IntersectingLines(d, sizeof(d)/sizeof(*d)) << '\n'; // expected output: "no intersection"
+int main() {
+  // string A[] = gets(stdin);
+  // cout << IntersectingLines(A, sizeof(A) / sizeof(*A));
+  string b[] = {"(3,0)", "(1,4)", "(0,-3)", "(2,3)"};
+  cout << IntersectingLines(b, sizeof(b) / sizeof(*b))
+       << '\n';  // expected output: "(9/5,12/5)"
+  string c[] = {"(9,-2)", "(-2,9)", "(3,4)", "(10,11)"};
+  cout << IntersectingLines(c, sizeof(c) / sizeof(*c))
+       << '\n';  // expected output: "(3,4)"
+  string d[] = {"(1,2)", "(3,4)", "(-5,-6)", "(-7,-8)"};
+  cout << IntersectingLines(d, sizeof(d) / sizeof(*d))
+       << '\n';  // expected output: "nointersection"
 
-  return 0;    
-} 
+  return 0;
+}
