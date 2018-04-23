218cb4fa032864aad3e2c244839d9a3de07dfb8d
Atila Budai <atib1980@gmail.com>
2018-04-23 08:08:42 +0200

    added find_longest_common_sequence_v3 fn

--- 3_hard_difficulty_level/27_lcs.cpp
+++ 3_hard_difficulty_level/27_lcs.cpp
@@ -24,7 +24,9 @@ Output: 3
 #include <cctype>
 #include <iostream>
 #include <locale>
+#include <queue>
 #include <string>
+#include <tuple>
 #include <vector>
 
 using namespace std;
@@ -49,6 +51,7 @@ size_t find_longest_common_sequence(const string& left_str,
 
   for (size_t i{}; i < left_str.length(); i++) {
     if (left_str.length() - i <= lcs)
+
       break;
 
     size_t prev_char_pos{};
@@ -116,9 +119,6 @@ void find_longest_common_sequence_v2(const string& left_str,
         lcs = current_lcs + 1;
     }
   }
-
-  if (current_lcs > lcs)
-    lcs = current_lcs;
 }
 
 string LCS_v2(string* str_arr, const size_t str_arr_size) {
@@ -131,29 +131,96 @@ string LCS_v2(string* str_arr, const size_t str_arr_size) {
   if (str_arr[0].empty() || str_arr[1].empty())
     return "not possible";
 
-  size_t max_lcs_len1{};
+  size_t lcs1{};
   find_longest_common_sequence_v2(str_arr[0], str_arr[0].length(), 0,
-                                  str_arr[1], max_lcs_len1, 0, 0);
-
-  size_t max_lcs_len2{};
+                                  str_arr[1], lcs1, 0, 0);
+  size_t lcs2{};
   find_longest_common_sequence_v2(str_arr[1], str_arr[1].length(), 0,
-                                  str_arr[0], max_lcs_len2, 0, 0);
+                                  str_arr[0], lcs2, 0, 0);
 
-  return max_lcs_len1 > max_lcs_len2 ? to_string(max_lcs_len1)
-                                     : to_string(max_lcs_len2);
+  const size_t max_lcs{lcs1 > lcs2 ? lcs1 : lcs2};
+  return to_string(max_lcs);
+}
+
+size_t find_longest_common_sequence_v3(const string& left_str,
+                                       const string& right_str) {
+  queue<tuple<size_t, size_t, size_t>> q{{make_tuple(0, 0, 0)}};
+  const size_t left_str_len{left_str.length()};
+  size_t max_lcs1{};
+
+  while (!q.empty()) {
+    const size_t pos{get<0>(q.front())};
+    const size_t current_lcs{get<1>(q.front())};
+    const size_t prev_char_pos{get<2>(q.front())};
+    q.pop();
+
+    for (size_t i{pos}; i < left_str_len; i++) {
+      if (left_str_len - i + current_lcs <= max_lcs1)
+        break;
+      const size_t next_char_pos{right_str.find(left_str[i], prev_char_pos)};
+
+      if (string::npos != next_char_pos) {
+        if (i + 1 < left_str_len)
+          q.emplace(make_tuple(i + 1, current_lcs + 1, next_char_pos + 1));
+        else if (current_lcs + 1 > max_lcs1)
+          max_lcs1 = current_lcs + 1;
+      }
+    }
+  }
+
+  q.emplace(make_tuple(0, 0, 0));
+  const size_t right_str_len{right_str.length()};
+  size_t max_lcs2{};
+
+  while (!q.empty()) {
+    const size_t pos{get<0>(q.front())};
+    const size_t current_lcs{get<1>(q.front())};
+    const size_t prev_char_pos{get<2>(q.front())};
+    q.pop();
+
+    for (size_t i{pos}; i < right_str_len; i++) {
+      if (right_str_len - i + current_lcs <= max_lcs2)
+        break;
+      const size_t next_char_pos{left_str.find(right_str[i], prev_char_pos)};
+
+      if (string::npos != next_char_pos) {
+        if (i + 1 < left_str_len)
+          q.emplace(make_tuple(i + 1, current_lcs + 1, next_char_pos + 1));
+        else if (current_lcs + 1 > max_lcs2)
+          max_lcs2 = current_lcs + 1;
+      }
+    }
+  }
+
+  return max_lcs1 > max_lcs2 ? max_lcs1 : max_lcs2;
+}
+
+string LCS_v3(string* str_arr, const size_t str_arr_size) {
+  if (!str_arr || str_arr_size < 2)
+    return "not possible";
+
+  str_arr[0] = trim(str_arr[0]);
+  str_arr[1] = trim(str_arr[1]);
+
+  if (str_arr[0].empty() || str_arr[1].empty())
+    return "not possible";
+
+  return to_string(find_longest_common_sequence_v3(str_arr[0], str_arr[1]));
 }
 
 int main() {
   // string A[] = gets(stdin);
-  // cout << LCS_v2(A, sizeof(A)/sizeof(*A));
+  // cout << LCS_v3(A, sizeof(A)/sizeof(*A));
   string B[] = {"abcabb", "bacb"};
-  cout << LCS_v2(B, sizeof(B) / sizeof(*B)) << '\n';  // expected output: "3"
+  cout << LCS_v3(B, sizeof(B) / sizeof(*B)) << '\n';  // expected output: "3"
   string C[] = {"abc", "cb"};
-  cout << LCS_v2(C, sizeof(C) / sizeof(*C)) << '\n';  // expected output: "1"
+  cout << LCS_v3(C, sizeof(C) / sizeof(*C)) << '\n';  // expected output: "1"
   string D[] = {"bcacb", "aacabb"};
-  cout << LCS_v2(D, sizeof(D) / sizeof(*D)) << '\n';  // expected output: "3"
+  cout << LCS_v3(D, sizeof(D) / sizeof(*D)) << '\n';  // expected output: "3"
   string E[] = {"a", "a"};
-  cout << LCS_v2(E, sizeof(E) / sizeof(*E)) << '\n';  // expected output: "1"
+  cout << LCS_v3(E, sizeof(E) / sizeof(*E)) << '\n';  // expected output: "1"
+  string F[] = {"aaac", "caaa"};
+  cout << LCS_v3(F, sizeof(F) / sizeof(*F)) << '\n';  // expected output: "3"
 
   return 0;
 }
