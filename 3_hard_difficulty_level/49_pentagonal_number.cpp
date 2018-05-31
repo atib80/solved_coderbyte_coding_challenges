b960bfa9c915207ac135d1c0ce506fab15da194a
Atila Budai <atib1980@gmail.com>
2018-05-31 14:15:18 +0200

    partial dynamic programming related solution

--- 3_hard_difficulty_level/49_pentagonal_number.cpp
+++ 3_hard_difficulty_level/49_pentagonal_number.cpp
@@ -1,11 +1,15 @@
 /*
 Coderbyte coding challenge: Pentagonal Number
 
-Using the C++ language, have the function PentagonalNumber(num) read num which will be a positive integer and determine how many dots exist in a pentagonal shape
-around a center dot on the Nth iteration. For example, in the image below you can see that on the first iteration there is only a single dot, 
-on the second iteration there are 6 dots, on the third there are 16 dots, and on the fourth there are 31 dots.
+Using the C++ language, have the function PentagonalNumber(num) read num which
+will be a positive integer and determine how many dots exist in a pentagonal
+shape around a center dot on the Nth iteration. For example, in the image below
+you can see that on the first iteration there is only a single dot, on the
+second iteration there are 6 dots, on the third there are 16 dots, and on the
+fourth there are 31 dots.
 
-Your program should return the number of dots that exist in the whole pentagon on the Nth iteration.
+Your program should return the number of dots that exist in the whole pentagon
+on the Nth iteration.
 
 Sample test cases:
 
@@ -17,38 +21,53 @@ Output: 51
 */
 
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 size_t pentagonal_number(const size_t number_of_iterations) {
+  static map<size_t, size_t> precalculated_pentagonal_numbers{
+      {1, 1}, {2, 6}, {3, 16}};
 
-  size_t number_of_dots{1};
-  size_t current_level_dots{5};
+  if (end(precalculated_pentagonal_numbers) !=
+      precalculated_pentagonal_numbers.find(number_of_iterations))
+    return precalculated_pentagonal_numbers.at(number_of_iterations);
 
-  for (size_t i{2u}; i <= number_of_iterations; i++) {
+  auto lb_iter =
+      precalculated_pentagonal_numbers.upper_bound(number_of_iterations);
 
-  	number_of_dots += current_level_dots;
+  if (lb_iter != end(precalculated_pentagonal_numbers) &&
+      lb_iter != begin(precalculated_pentagonal_numbers))
+    --lb_iter;
 
-  	current_level_dots += 5;
+  size_t number_of_dots{
+      lb_iter != end(precalculated_pentagonal_numbers) ? lb_iter->second : 1};
+  size_t current_level_dots{lb_iter != end(precalculated_pentagonal_numbers)
+                                ? lb_iter->first * 5
+                                : 5};
+  size_t i{lb_iter != end(precalculated_pentagonal_numbers) ? lb_iter->first + 1
+                                                            : 2};
 
+  for (; i <= number_of_iterations; i++) {
+    number_of_dots += current_level_dots;
+    current_level_dots += 5;
   }
 
-   
-  return number_of_dots; 
-            
+  precalculated_pentagonal_numbers[number_of_iterations] = number_of_dots;
+
+  return number_of_dots;
 }
 
 int main() {
-  
-	// cout << pentagonal_number(gets(stdin));	
-	cout << pentagonal_number(1) << '\n'; // expected output: 1
-	cout << pentagonal_number(2) << '\n'; // expected output: 6
-	cout << pentagonal_number(3) << '\n'; // expected output: 16
-	cout << pentagonal_number(4) << '\n'; // expected output: 31
-	cout << pentagonal_number(5) << '\n'; // expected output: 51
-	cout << pentagonal_number(6) << '\n'; // expected output: 76
-	cout << pentagonal_number(7) << '\n'; // expected output: 106
-	cout << pentagonal_number(8) << '\n'; // expected output: 141
-	
-	return 0;    
-} 
+  // cout << pentagonal_number(gets(stdin));
+  cout << pentagonal_number(1) << '\n';  // expected output: 1
+  cout << pentagonal_number(2) << '\n';  // expected output: 6
+  cout << pentagonal_number(3) << '\n';  // expected output: 16
+  cout << pentagonal_number(4) << '\n';  // expected output: 31
+  cout << pentagonal_number(5) << '\n';  // expected output: 51
+  cout << pentagonal_number(6) << '\n';  // expected output: 76
+  cout << pentagonal_number(7) << '\n';  // expected output: 106
+  cout << pentagonal_number(8) << '\n';  // expected output: 141
+
+  return 0;
+}
