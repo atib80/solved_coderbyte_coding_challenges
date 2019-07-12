34b13ef43e80af32f183eaa1d7e91fca7e228ae5
Attila Budai <atib@eunet.rs>
2019-07-12 18:26:37 +0200

    optimization using constexpr context

--- 1_easy_difficulty_level/09_time_convert.cpp
+++ 1_easy_difficulty_level/09_time_convert.cpp
@@ -16,11 +16,16 @@
  */
 
 #include <iostream>
-#include <string>
 
-std::string TimeConvert(size_t minutes) {
+static char buffer[16]{};
+static constexpr const size_t buf_len{sizeof(buffer) / sizeof(buffer[0])};
+
+constexpr const char* TimeConvert(size_t minutes) noexcept {
+
   if (minutes < 60) {
-    static char buffer[5]{"0:"};
+    buffer[0] = '0';
+    buffer[1] = ':';
+
     if (minutes < 10) {
       buffer[2] = '0' + minutes;
       buffer[3] = 0;
@@ -34,7 +39,32 @@ std::string TimeConvert(size_t minutes) {
     return buffer;
   }
 
-  return std::to_string(minutes / 60) + ":" + std::to_string(minutes % 60);
+  size_t hours{minutes / 60}, index{buf_len - 1};
+  minutes %= 60;
+
+  while (hours > 0) {
+    buffer[index--] = '0' + hours % 10;
+    hours /= 10;
+  }
+
+  for (size_t i{}, j{index + 1}; j < buf_len; ++i, ++j) {
+    buffer[i] = buffer[j];
+    index = i;
+  }
+
+  buffer[++index] = ':';
+
+  size_t offset = minutes > 9 ? 2 : 1;
+
+  buffer[index + offset + 1] = 0;
+
+  buffer[index + offset] = '0' + minutes % 10;
+  minutes /= 10;
+  --offset;
+  if (minutes > 0)
+    buffer[index + offset] = '0' + minutes;
+
+  return buffer;
 }
 
 int main() {
@@ -42,6 +72,8 @@ int main() {
   std::cout << TimeConvert(120) << '\n';  // expected output: "2:0"
   std::cout << TimeConvert(35) << '\n';   // expected output: "0:35"
   std::cout << TimeConvert(256) << '\n';  // expected output: "4:16"
+  std::cout << TimeConvert(308) << '\n';  // expected output: "5:08"
+  std::cout << TimeConvert(935) << '\n';  // expected output: "15:35"
 
   return 0;
 }
