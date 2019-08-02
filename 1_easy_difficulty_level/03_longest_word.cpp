86a2970d868e435f81aa80818d4617b8cdd3de47
Attila Budai <atib@eunet.rs>
2019-08-02 22:52:19 +0200

    bug fix: pass by forwarding ref, testing 3 different containers

--- 1_easy_difficulty_level/03_longest_word.cpp
+++ 1_easy_difficulty_level/03_longest_word.cpp
@@ -16,12 +16,16 @@ Output: "love"
 */
 
 #include <algorithm>
+#include <cassert>
 #include <cctype>
 #include <iostream>
 #include <iterator>
+#include <list>
+#include <random>
 #include <string>
 #include <type_traits>
 #include <unordered_set>
+#include <vector>
 
 using std::array;
 using std::begin;
@@ -30,16 +34,26 @@ using std::cbegin;
 using std::cend;
 using std::cout;
 using std::declval;
+using std::distance;
 using std::end;
 using std::false_type;
+using std::find;
+using std::find_if;
+using std::forward;
 using std::iterator_traits;
+using std::list;
 using std::make_pair;
 using std::move;
+using std::mt19937;
 using std::pair;
+using std::random_device;
+using std::remove_reference_t;
+using std::shuffle;
 using std::sort;
 using std::string;
 using std::true_type;
 using std::unordered_set;
+using std::vector;
 using std::void_t;
 
 string LongestWord_v1(string sen) {
@@ -212,9 +226,11 @@ pair<ForwardIterType, ForwardIterType> find_next_sequence(
 }
 
 template <typename StringType, typename ContainerType>
-StringType find_longest_word(StringType src, const ContainerType& haystack) {
-  using T = typename StringType::value_type;
-  using U = typename ContainerType::value_type;
+std::remove_reference_t<StringType> find_longest_word(
+    StringType&& src,
+    ContainerType&& haystack) {
+  using T = typename remove_reference_t<StringType>::value_type;
+  using U = typename remove_reference_t<ContainerType>::value_type;
 
   bool is_haystack_sorted{has_find_member_function_v<ContainerType, T>};
 
@@ -225,15 +241,15 @@ StringType find_longest_word(StringType src, const ContainerType& haystack) {
     if constexpr (has_sort_member_function_v<ContainerType>)
       haystack.sort();
     else
-      sort(begin(haystack),
-           end(haystack));
+      sort(begin(forward<ContainerType>(haystack)),
+           end(forward<ContainerType>(haystack)));
     is_haystack_sorted = true;
   }
 
-  auto next_iter = cbegin(src);
-  const auto last_iter = cend(src);
+  auto next_iter = cbegin(forward<StringType>(src));
+  const auto last_iter = cend(forward<StringType>(src));
   decltype(distance(next_iter, last_iter)) max_word_len{};
-  StringType longest_first_word{};
+  remove_reference_t<StringType> longest_first_word{};
 
   while (next_iter != last_iter) {
     const auto [first, last] =
@@ -256,11 +272,36 @@ StringType find_longest_word(StringType src, const ContainerType& haystack) {
 }
 
 string LongestWord_v3(string sen) {
+  static std::random_device rd{};
   static constexpr const array<char, 63> allowed_chars{
       {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"}};
 
-  return find_longest_word(move(sen), unordered_set<char>{cbegin(allowed_chars),
-                                                          cend(allowed_chars)});
+  vector<char> unsorted_allowed_chars{cbegin(allowed_chars),
+                                      cend(allowed_chars)};
+  std::mt19937 generator_functor(rd());
+  shuffle(begin(unsorted_allowed_chars), end(unsorted_allowed_chars),
+          generator_functor);
+
+  list<char> list_of_unsorted_chars{cbegin(unsorted_allowed_chars),
+                                    cend(unsorted_allowed_chars)};
+
+  unordered_set<char> allowed_chars_hash_set{cbegin(allowed_chars),
+                                             cend(allowed_chars)};
+  const string longest_word1{find_longest_word(sen, allowed_chars_hash_set)};
+  const string longest_word2{find_longest_word(sen, unsorted_allowed_chars)};
+  const string longest_word3{find_longest_word(sen, list_of_unsorted_chars)};
+
+  assert(longest_word1.length() == longest_word2.length());
+  assert(longest_word1.length() == longest_word3.length());
+  assert(longest_word2.length() == longest_word3.length());
+
+  return longest_word1.length() >= longest_word2.length()
+             ? (longest_word1.length() >= longest_word3.length()
+                    ? longest_word1
+                    : longest_word3)
+             : (longest_word2.length() >= longest_word3.length()
+                    ? longest_word2
+                    : longest_word3);
 }
 
 int main() {
