3ef85067d69697852256b0fa55fde6c1e72b1369
Attila Budai <atib@eunet.rs>
2019-08-09 13:55:13 +0200

    optimal 2nd solution using consexpr dict

--- 1_easy_difficulty_level/12_vowel_count.cpp
+++ 1_easy_difficulty_level/12_vowel_count.cpp
@@ -15,6 +15,8 @@ Input:  "coderbyte"
 Output: 3
 */
 
+#include <algorithm>
+#include <array>
 #include <deque>
 #include <iostream>
 #include <string>
@@ -25,7 +27,7 @@ using namespace std;
 string trim(const string& str) {
   const size_t str_len{str.length()};
 
-  if (!str_len)
+  if (0U == str_len)
     return {};
 
   size_t begin_str{};
@@ -48,21 +50,24 @@ string trim(const string& str) {
 }
 
 string int2str(long long number) {
-  deque<char> buffer{};  
+  static char buffer[16]{};
   const bool is_negative{number < 0};
 
+  size_t index{};
+
   while (number) {
-    buffer.push_front('0' + number % 10);
+    buffer[index++] = '0' + number % 10;
     number /= 10;
   }
 
   if (is_negative)
-    buffer.push_front('-');
+    buffer[index++] = '-';
 
-  return string{cbegin(buffer), cend(buffer)};
+  std::reverse(buffer, buffer + index);
+  return string{buffer, buffer + index};
 }
 
-string vowel_count(string str) {
+string vowel_count_v1(string str) {
   const unordered_set<char> vowels{'A', 'E', 'I', 'O', 'U',
                                    'a', 'e', 'i', 'o', 'u'};
 
@@ -77,12 +82,50 @@ string vowel_count(string str) {
   return int2str(v_count);
 }
 
+template <size_t BUFFER_LENGTH>
+class vowel_indices {
+  int vowels[BUFFER_LENGTH];
+
+ public:
+  constexpr vowel_indices() : vowels{} {
+    for (const char index : "aeiouAEIOU")
+      vowels[static_cast<size_t>(index)] = 1;
+  }
+
+  int operator[](const size_t index) const {
+    if (index < BUFFER_LENGTH)
+      return vowels[index];
+    static char buffer[128]{};
+    snprintf(buffer, 128, "index's value must be between 0 and %lu!",
+             BUFFER_LENGTH);
+    throw out_of_range{buffer};
+  }
+};
+
+static constexpr vowel_indices<128> char_dict{};
+
+string vowel_count_v2(string str) {
+  str = trim(str);
+  size_t v_count{};
+
+  for (const auto ch : str) {
+    if (char_dict[ch])
+      v_count++;
+  }
+
+  return int2str(v_count);
+}
+
 int main() {
-  // cout << vowel_count(gets(stdin));
-  cout << vowel_count("All cows eat grass and moo")
-       << '\n';                              // expected output: 8
-  cout << vowel_count("hello") << '\n';      // expected output: 2
-  cout << vowel_count("coderbyte") << '\n';  // expected output: 3
+  // cout << vowel_count_v2(gets(stdin));
+  cout << vowel_count_v2("All cows eat grass and moo")
+       << '\n';                                 // expected output: 8
+  cout << vowel_count_v2("hello") << '\n';      // expected output: 2
+  cout << vowel_count_v2("coderbyte") << '\n';  // expected output: 3
+  cout << vowel_count_v2(
+              "Today is a perfect day for taking a long walk and finding a "
+              "good spot for swimming.")
+       << '\n';  // expected output: 23
 
   return 0;
 }
