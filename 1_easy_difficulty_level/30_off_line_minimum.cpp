c33b19eaab1916ae920b40d2194e934761164151
Attila Budai <atib@eunet.rs>
2019-09-20 22:18:49 +0200

    improved solutions, optimization

--- 1_easy_difficulty_level/30_off_line_minimum.cpp
+++ 1_easy_difficulty_level/30_off_line_minimum.cpp
@@ -20,7 +20,6 @@ Output: "4,1,2,3"
 */
 
 #include <algorithm>
-#include <cctype>
 #include <functional>
 #include <iostream>
 #include <queue>
@@ -31,116 +30,75 @@ Output: "4,1,2,3"
 
 using namespace std;
 
-string trim(const string& str) {
-  const size_t str_len{str.length()};
-
-  if (!str_len)
-    return string{};
-
-  size_t first{}, last{str_len - 1};
-
-  for (; first <= last; ++first) {
-    if (!isspace(str[first]))
-      break;
-  }
-
-  if (first > last)
-    return string{};
-
-  for (; last > first; --last) {
-    if (!isspace(str[last]))
-      break;
-  }
-
-  return str.substr(first, last - first + 1);
-}
-
 string OffLineMinimum_v1(vector<string> str_arr) {
-  
-  multiset<long> numbers{};
-  vector<long> extracted_numbers{};
-
-  for (auto& item : str_arr) {
-    item = trim(item);
-
-    if ((item == "E") && (!numbers.empty())) {
-      extracted_numbers.emplace_back(*begin(numbers));
-      numbers.erase(begin(numbers));
-      continue;
-    }
-
-    numbers.insert(stol(item));
-  }
-
-  ostringstream oss{};
-
-  for (size_t i{}; i < extracted_numbers.size() - 1; i++) {
-    oss << extracted_numbers[i] << ',';
-  }
-
-  oss << extracted_numbers.back();
-
-  return oss.str();
-}
-
-string OffLineMinimum_v2(vector<string> str_arr) {
-
-  vector<long> numbers{};
+  multiset<int64_t> numbers{};
   ostringstream oss{};
 
   for (auto& item : str_arr) {
-    item = trim(item);
-
-    if ((item == "E") && (!numbers.empty())) {
-      make_heap(begin(numbers), end(numbers), greater<long>{});
-      pop_heap(begin(numbers), end(numbers), greater<long>{});
-      oss << numbers.back() << ',';
-      numbers.pop_back();
-      continue;
-    }
-
-    numbers.emplace_back(stol(item));
-    push_heap(begin(numbers), end(numbers), greater<long>{});
+    if (item == "E") {
+      if (!numbers.empty()) {
+        oss << *begin(numbers) << ',';
+        numbers.erase(cbegin(numbers));
+      }
+    } else
+      numbers.insert(stoll(item));
   }
 
   string output{oss.str()};
-  output.erase(output.length() - 1, 1);
+  output.erase(--cend(output));
+  return output;
+}
+
+string OffLineMinimum_v2(vector<string> str_arr) {
+  vector<int64_t> numbers{};
+  ostringstream oss{};
+
+  for (auto& item : str_arr) {
+    if (item == "E") {
+      if (!numbers.empty()) {
+        make_heap(begin(numbers), end(numbers), greater<int64_t>{});
+        pop_heap(begin(numbers), end(numbers), greater<int64_t>{});
+        oss << numbers.back() << ',';
+        numbers.pop_back();
+      }
+    } else {
+      numbers.emplace_back(stoll(item));
+      push_heap(begin(numbers), end(numbers), greater<int64_t>{});
+    }
+  }
+
+  string output{oss.str()};
+  output.erase(--cend(output));
   return output;
 }
 
 string OffLineMinimum_v3(vector<string> str_arr) {
-
-  priority_queue<long, vector<long>, greater<long>> numbers{};
+  priority_queue<int64_t, vector<int64_t>, greater<int64_t>> numbers{};
   ostringstream oss{};
 
   for (auto& item : str_arr) {
-    item = trim(item);
-
-    if ((item == "E") && (!numbers.empty())) {
-      oss << numbers.top() << ',';
-      numbers.pop();
-      continue;
-    }
-
-    numbers.emplace(stol(item));
+    if (item == "E") {
+      if (!numbers.empty()) {
+        oss << numbers.top() << ',';
+        numbers.pop();
+      }
+    } else
+      numbers.emplace(stoll(item));
   }
 
   string output{oss.str()};
-  output.erase(output.length() - 1, 1);
+  output.erase(--cend(output));
   return output;
 }
 
 int main() {
-  // cout << OffLineMinimum_v2(move(vector<string>{gets(stdin)}));
-  cout << OffLineMinimum_v2(move(
-              vector<string>{"5", "4", "6", "E", "1", "7", "E", "E", "3", "2"}))
+  // cout << OffLineMinimum_v1(gets(stdin));
+  cout << OffLineMinimum_v1({"5", "4", "6", "E", "1", "7", "E", "E", "3", "2"})
        << '\n';
   // expected output: 4,1,5
-  cout << OffLineMinimum_v2(move(vector<string>{"1", "2", "E", "E", "3"}))
+  cout << OffLineMinimum_v1({"1", "2", "E", "E", "3"})
        << '\n';  // expected output: 1,2
-  cout << OffLineMinimum_v2(
-              move(vector<string>{"4", "E", "1", "E", "2", "E", "3", "E"}))
-       << '\n';
+  cout << OffLineMinimum_v1({"4", "E", "1", "E", "2", "E", "3", "E"}) << '\n';
   // expected output: 4,1,2,3
 
   return 0;
