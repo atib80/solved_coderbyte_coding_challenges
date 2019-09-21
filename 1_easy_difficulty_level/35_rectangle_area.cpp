ad84731b8aca5780c8efd080d2ecb1dcf9f62f48
Attila Budai <atib@eunet.rs>
2019-09-22 00:01:37 +0200

    bug fix, simplified, optimized solution

--- 1_easy_difficulty_level/35_rectangle_area.cpp
+++ 1_easy_difficulty_level/35_rectangle_area.cpp
@@ -19,155 +19,83 @@ Output: 1
 */
 
 #include <algorithm>
-#include <cctype>
-#include <functional>
+#include <array>
 #include <iostream>
-#include <set>
 #include <string>
-#include <utility>
 #include <vector>
 
 using namespace std;
 
-string trim(const string& input) {
-  string output{input};
-  output.erase(begin(output),
-               find_if(begin(output), end(output),
-                       [](const char ch) { return !isspace(ch); }));
-
-  output.erase(find_if(output.rbegin(), output.rend(),
-                       [](const char ch) { return !isspace(ch); })
-                   .base(),
-               end(output));
-
-  return output;
-}
-
-vector<string> split(const string& source,
-                     const char* needle,
-                     size_t const max_count = string::npos) {
-  vector<string> parts{};
-
-  string needle_st{needle};
-
-  const size_t source_len{source.length()};
-
-  const size_t needle_len{needle_st.length()};
-
-  if (!source_len)
-    return parts;
-
-  if (!needle_len) {
-    const size_t upper_limit{max_count < source_len ? max_count : source_len};
-    for (size_t i{}; i < upper_limit; i++)
-      parts.emplace_back(1, source[i]);
-    return parts;
-  }
-
-  size_t number_of_parts{}, prev{};
-
-  while (true) {
-    const size_t current{source.find(needle_st, prev)};
-
-    if (string::npos == current)
-      break;
-
-    number_of_parts++;
-
-    if (string::npos != max_count && parts.size() == max_count)
-      break;
-
-    if (current - prev > 0)
-      parts.emplace_back(source.substr(prev, current - prev));
-
-    prev = current + needle_len;
-
-    if (prev >= source_len)
-      break;
-  }
-
-  if (prev < source_len) {
-    if (string::npos == max_count)
-      parts.emplace_back(source.substr(prev));
-
-    else if (parts.size() < max_count)
-      parts.emplace_back(source.substr(prev));
-  }
-
-  return parts;
-}
-
-pair<int, int> parse_x_y_coordinate_pair(const string& str) {
+pair<int64_t, int64_t> parse_x_y_coordinate_pair(const string& str) {
   size_t start_pos{str.find_first_of("0123456789")};
 
   if (string::npos == start_pos)
-    return make_pair(0, 0);
+    return {0, 0};
 
   size_t end_pos{str.find_first_not_of("0123456789", start_pos + 1)};
 
   if (string::npos == end_pos)
-    return make_pair(0, 0);
+    return {0, 0};
 
-  const int x{stoi(str.substr(start_pos, end_pos - start_pos))};
+  const int64_t x{stoll(str.substr(start_pos, end_pos - start_pos))};
 
   start_pos = str.find_first_of("0123456789", end_pos + 1);
 
   if (string::npos == start_pos)
-    return make_pair(0, 0);
+    return {0, 0};
 
   end_pos = str.find_first_not_of("0123456789", start_pos + 1);
 
-  const int y{string::npos != end_pos
-                  ? stoi(str.substr(start_pos, end_pos - start_pos))
-                  : stoi(str.substr(start_pos))};
+  if (string::npos == end_pos)
+    end_pos = str.length();
 
-  return make_pair(x, y);
+  const int64_t y{stoll(str.substr(start_pos, end_pos - start_pos))};
+
+  return {x, y};
 }
 
 string RectangleArea(vector<string> xy_coordinate_pairs) {
-  if (4 != xy_coordinate_pairs.size())
-    return "Not possible!";
+  if (xy_coordinate_pairs.size() < 4U)
+    return "not possible";
 
-  vector<pair<int, int>> coordinates{};
+  std::array<int64_t, 4U> x_coordinates;
+  std::array<int64_t, 4U> y_coordinates;
 
-  for (const auto& xy_str : xy_coordinate_pairs) {
-    coordinates.emplace_back(parse_x_y_coordinate_pair(xy_str));
-  }
+  pair<int64_t, int64_t> coord{
+      parse_x_y_coordinate_pair(xy_coordinate_pairs[0])};
+  x_coordinates[0] = coord.first;
+  y_coordinates[0] = coord.second;
 
-  set<int, greater<int>> x_coordinates{};
-  set<int, greater<int>> y_coordinates{};
+  coord = parse_x_y_coordinate_pair(xy_coordinate_pairs[1]);
+  x_coordinates[1] = coord.first;
+  y_coordinates[1] = coord.second;
 
-  for (const auto& xy_pair : coordinates) {
-    x_coordinates.insert(xy_pair.first);
-    y_coordinates.insert(xy_pair.second);
-  }
+  coord = parse_x_y_coordinate_pair(xy_coordinate_pairs[2]);
+  x_coordinates[2] = coord.first;
+  y_coordinates[2] = coord.second;
 
-  if (2 != x_coordinates.size() || 2 != y_coordinates.size())
-    return "0";
+  coord = parse_x_y_coordinate_pair(xy_coordinate_pairs[3]);
+  x_coordinates[3] = coord.first;
+  y_coordinates[3] = coord.second;
 
-  const auto first_x = begin(x_coordinates);
-  auto second_x = first_x;
-  ++second_x;
-  const int width{*first_x - *second_x};
+  const auto xmin_xmax{
+      minmax_element(cbegin(x_coordinates), cend(x_coordinates))};
+  const auto ymin_ymax{
+      minmax_element(cbegin(y_coordinates), cend(y_coordinates))};
 
-  const auto first_y = begin(y_coordinates);
-  auto second_y = first_y;
-  ++second_y;
-  const int height{*first_y - *second_y};
+  const int64_t width{*(xmin_xmax.second) - *(xmin_xmax.first)};
+  const int64_t height{*(ymin_ymax.second) - *(ymin_ymax.first)};
 
   return to_string(width * height);
 }
 
 int main() {
-  // cout << RectangleArea(move(vector<string>{gets(stdin)}));
-  cout << RectangleArea(
-              move(vector<string>{"(0 0)", "(3 0)", "(0 2)", "(3 2)"}))
+  // cout << RectangleArea(gets(stdin));
+  cout << RectangleArea({"(0 0)", "(3 0)", "(0 2)", "(3 2)"})
        << '\n';  // expected output: "6"
-  cout << RectangleArea(
-              move(vector<string>{"(1 1)", "(1 3)", "(3 1)", "(3 3)"}))
+  cout << RectangleArea({"(1 1)", "(1 3)", "(3 1)", "(3 3)"})
        << '\n';  // expected output: "4"
-  cout << RectangleArea(
-              move(vector<string>{"(0 0)", "(1 0)", "(1 1)", "(0 1)"}))
+  cout << RectangleArea({"(0 0)", "(1 0)", "(1 1)", "(0 1)"})
        << '\n';  // expected output: "1"
 
   return 0;
