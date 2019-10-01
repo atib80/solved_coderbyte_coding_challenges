cd9298653f8b13e3504143f8c518ec0075d8fbf1
Attila Budai <atib@eunet.rs>
2019-10-01 09:53:39 +0200

    update

--- 1_easy_difficulty_level/49_product_digits.cpp
+++ 1_easy_difficulty_level/49_product_digits.cpp
@@ -25,28 +25,31 @@ Output: 3
 
 using namespace std;
 
-size_t get_number_length(size_t number) {
+constexpr size_t get_number_length(size_t number) {
   size_t len{};
 
-  while (number) {
+  while (0U != number) {
     number /= 10;
-    len++;
+    ++len;
   }
 
   return len;
 }
 
 size_t ProductDigits_v1(const size_t num) {
-  string result{"1" + to_string(num)};
-  size_t min_len{result.length()};
+  const size_t num_len{to_string(num).length()};
+  size_t min_len{num_len + 1};
 
-  for (size_t i{2}; i < static_cast<size_t>(sqrt(num)) + 1; i++) {
-    const size_t factor{num / i};
-
-    if (num == i * factor) {
-      result = to_string(i) + to_string(factor);
-      if (result.length() < min_len)
+  for (size_t i{2U}; i < static_cast<size_t>(sqrt(num)) + 1; ++i) {
+    if (num % i == 0U) {
+      const size_t factor{num / i};
+      const string result{to_string(i) + to_string(factor)};
+      if (result.length() < min_len) {
         min_len = result.length();
+        if (num_len == min_len)
+          return min_len;
+      } else if (result.length() > min_len)
+        break;
     }
   }
 
@@ -54,16 +57,20 @@ size_t ProductDigits_v1(const size_t num) {
 }
 
 size_t ProductDigits_v2(const size_t num) {
-  size_t min_len{1 + get_number_length(num)};
+  const size_t num_len{get_number_length(num)};
+  size_t min_len{num_len + 1};
 
-  for (size_t i{2}; i < static_cast<size_t>(sqrt(num)) + 1; i++) {
-    const size_t factor{num / i};
-
-    if (num == i * factor) {
+  for (size_t i{2U}; i < static_cast<size_t>(sqrt(num)) + 1; ++i) {
+    if (num % i == 0U) {
+      const size_t factor{num / i};
       const size_t current_len{get_number_length(i) +
                                get_number_length(factor)};
-      if (current_len < min_len)
+      if (current_len < min_len) {
         min_len = current_len;
+        if (num_len == min_len)
+          return min_len;
+      } else if (current_len > min_len)
+        break;
     }
   }
 
@@ -72,12 +79,14 @@ size_t ProductDigits_v2(const size_t num) {
 
 int main() {
   // cout << ProductDigits_v2(gets(stdin));
-  cout << ProductDigits_v2(24) << '\n';  // expected output: 2
-  cout << ProductDigits_v2(90) << '\n';  // expected output: 3
-  cout << ProductDigits_v2(6) << '\n';   // expected output: 2
-  cout << ProductDigits_v2(23) << '\n';  // expected output: 3
-  cout << ProductDigits_v2(1) << '\n';   // expected output: 2
-  cout << ProductDigits_v2(79) << '\n';  // expected output: 3
+  cout << ProductDigits_v2(24) << '\n';    // expected output: 2
+  cout << ProductDigits_v2(90) << '\n';    // expected output: 3
+  cout << ProductDigits_v2(6) << '\n';     // expected output: 2
+  cout << ProductDigits_v2(23) << '\n';    // expected output: 3
+  cout << ProductDigits_v2(1) << '\n';     // expected output: 2
+  cout << ProductDigits_v2(79) << '\n';    // expected output: 3
+  cout << ProductDigits_v2(1764) << '\n';  // expected output: 4
+  cout << ProductDigits_v2(1024) << '\n';  // expected output: 4
 
   return 0;
 }
