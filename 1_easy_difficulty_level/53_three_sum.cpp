e5384caeec9756389e5ce5099a23a5fb9c1b84da
Attila Budai <atib@eunet.rs>
2019-10-15 23:31:55 +0200

    ThreeSum_v1, ThreeSum_v2 fn use binary_search, upper_bound

--- 1_easy_difficulty_level/53_three_sum.cpp
+++ 1_easy_difficulty_level/53_three_sum.cpp
@@ -20,49 +20,27 @@ Output: "false"
 */
 
 #include <algorithm>
-#include <functional>
 #include <iostream>
-#include <set>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-string ThreeSum_v1(const int* arr, const size_t arr_size) {
-  if (arr_size < 4)
+string ThreeSum_v1(int* arr, const size_t arr_size) {
+  if (arr_size < 4U)
     return "false";
 
   const int sum{arr[0]};
+  sort(arr + 1, arr + arr_size);
 
-  set<int, greater<int>> sorted_sequence(arr + 1, arr + arr_size);
-
-  vector<int> numbers(begin(sorted_sequence), end(sorted_sequence));
-  sort(begin(numbers), end(numbers), [](const int l, const int r) {
-
-    return l > r;
-
-  });
-
-  const bool no_negative_numbers{
-      end(numbers) ==
-      find_if(begin(numbers), end(numbers), [](const int n) { return n < 0; })};
-
-  const size_t ns{numbers.size()};
-
-  if (ns < 3)
-    return "false";
-
-  for (size_t i{}; i < ns - 2; i++) {
-    for (size_t j{i + 1}; j < ns - 1; j++) {
-      for (size_t k{j + 1}; k < ns; k++) {
-        const int current_sum{numbers[i] + numbers[j] + numbers[k]};
-
-        if (sum == current_sum)
-          return "true";
-
-        if (no_negative_numbers && (current_sum < sum))
-          return "false";
-      }
+  for (size_t i{1}; i < arr_size - 2;
+       i = static_cast<size_t>(
+           upper_bound(arr + i + 1, arr + arr_size - 2, arr[i]) - arr)) {
+    for (size_t j{i + 1}; j < arr_size - 1;
+         j = static_cast<size_t>(
+             upper_bound(arr + j + 1, arr + arr_size - 1, arr[j]) - arr)) {
+      const auto current_sum{arr[i] + arr[j]};
+      if (binary_search(arr + j + 1, arr + arr_size, sum - current_sum))
+        return "true";
     }
   }
 
@@ -72,53 +50,57 @@ string ThreeSum_v1(const int* arr, const size_t arr_size) {
 bool check_if_three_numbers_exist(const int* arr,
                                   const size_t arr_size,
                                   const size_t pos,
-                                  const size_t iter_count = 1,
+                                  const size_t iter_count = 1U,
                                   const int current_sum = 0) {
-  for (size_t i{pos}; i < arr_size; i++) {
-    if ((current_sum + arr[i] == arr[0]) && (3 == iter_count))
-      return true;
+  if (3U == iter_count)
+    return binary_search(arr + pos, arr + arr_size, arr[0] - current_sum);
 
-    else if (iter_count < 3) {
-      if (check_if_three_numbers_exist(arr, arr_size, i + 1, iter_count + 1,
-                                       current_sum + arr[i]))
-        return true;
-    }
+  if (current_sum + arr[pos] > arr[0])
+    return false;
+
+  for (size_t i{pos}; i < arr_size;
+       i = static_cast<size_t>(
+           upper_bound(arr + i + 1, arr + arr_size, arr[i]) - arr)) {
+    if (check_if_three_numbers_exist(arr, arr_size, i + 1, iter_count + 1,
+                                     current_sum + arr[i]))
+      return true;
   }
 
   return false;
 }
 
-string ThreeSum_v2(const int* arr, const size_t arr_size) {
-  if (arr_size < 4)
+string ThreeSum_v2(int* arr, const size_t arr_size) {
+  if (arr_size < 4U)
     return "false";
 
-  if (check_if_three_numbers_exist(arr, arr_size, 1, 1, 0))
+  sort(arr + 1, arr + arr_size);
+  if (check_if_three_numbers_exist(arr, arr_size, 1U, 1U, 0))
     return "true";
 
   return "false";
 }
 
 int main() {
-  // const int A[] = gets(stdin);
-  // cout << ThreeSum_v2(A, sizeof(A)/sizeof(*A));
-  const int B[] = {8, 2, 1, 4, 10, 5, -1, -1};
-  cout << ThreeSum_v2(B, sizeof(B) / sizeof(*B))
+  // int A[] = gets(stdin);
+  // cout << ThreeSum_v1(A, sizeof(A)/sizeof(*A));
+  int B[]{8, 2, 1, 4, 10, 5, -1, -1};
+  cout << ThreeSum_v1(B, sizeof(B) / sizeof(*B))
        << '\n';  // expected output: "true"
-  const int C[] = {10, 2, 3, 1, 5, 3, 1, 4, -4, -3, -2};
-  cout << ThreeSum_v2(C, sizeof(C) / sizeof(*C))
+  int C[]{10, 2, 3, 1, 5, 3, 1, 4, -4, -3, -2};
+  cout << ThreeSum_v1(C, sizeof(C) / sizeof(*C))
        << '\n';  // expected output: "true"
-  const int D[] = {12, 3, 1, -5, -4, 7};
-  cout << ThreeSum_v2(D, sizeof(D) / sizeof(*D))
+  int D[]{12, 3, 1, -5, -4, 7};
+  cout << ThreeSum_v1(D, sizeof(D) / sizeof(*D))
        << '\n';  // expected output: "false"
-  const int E[] = {10, 5, 5, 5, 5};
-  cout << ThreeSum_v2(E, sizeof(E) / sizeof(*E))
+  int E[]{10, 5, 5, 5, 5};
+  cout << ThreeSum_v1(E, sizeof(E) / sizeof(*E))
        << '\n';  // expected output: "false"
-  const int F[] = {10, 2, 2, 2, 2, 2, 2,  2,  2,  2,  2,  5,
-                   5,  5, 5, 5, 5, 8, -2, -2, -2, -2, -1, 7};
-  cout << ThreeSum_v2(F, sizeof(F) / sizeof(*F))
+  int F[]{10, 2, 2, 2, 2, 2, 2,  2,  2,  2,  2,  5,
+          5,  5, 5, 5, 5, 8, -2, -2, -2, -2, -1, 7};
+  cout << ThreeSum_v1(F, sizeof(F) / sizeof(*F))
        << '\n';  // expected output: "true"
-  const int G[] = {5, -6, 4, -5, -3, -2, 7, 1, 2, 12, 8, 14};
-  cout << ThreeSum_v2(G, sizeof(G) / sizeof(*G))
+  int G[]{5, -6, 4, -5, -3, -2, 7, 1, 2, 12, 8, 14};
+  cout << ThreeSum_v1(G, sizeof(G) / sizeof(*G))
        << '\n';  // expected output: "true"
 
   return 0;
