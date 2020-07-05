45bb4316196f1e987f8ebb2e53c9ddf488b8629f
Attila Budai <atib1980@gmail.com>
2020-07-05 19:28:37 +0200

    added 3 alternative simple solutions (v2, v3, v4)

--- 1_easy_difficulty_level/85_fizzbuzz.cpp
+++ 1_easy_difficulty_level/85_fizzbuzz.cpp
@@ -20,15 +20,17 @@ Input:  8
 Output: "1 2 Fizz 4 Buzz Fizz 7 8"
 */
 
+#include <array>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 
-string FizzBuzz(const int num) {
-  if (num < 1)
-    return {};
-
+// ***** beginning of 1st solution *****
+string FizzBuzz_v1(const int num) {
   ostringstream oss{};
   oss << "1";
   for (int i{2}; i <= num; ++i) {
@@ -44,13 +46,94 @@ string FizzBuzz(const int num) {
 
   return oss.str();
 }
+// ***** end of 1st solution *****
+
+// ***** beginning of 2nd solution *****
+string FizzBuzz_v2(const int number) {
+  ostringstream oss{};
+  oss << "1";
+
+  for (int i{2}; i <= number; ++i) {
+    const bool divisible_by_3{0 == i % 3};
+    const bool divisible_by_5{0 == i % 5};
+    if (divisible_by_3) {
+      oss << (divisible_by_5 ? " FizzBuzz" : " Fizz");
+    } else if (divisible_by_5)
+      oss << " Buzz";
+    else
+      oss << ' ' << i;
+  }
+
+  return oss.str();
+}
+// ***** end of 2nd solution *****
+
+// ***** beginning of 3rd solution *****
+string FizzBuzz_v3(const int number) {
+  constexpr const array<const char*, 16> fizz_buzz_dict{
+      {nullptr, nullptr, nullptr, "Fizz", nullptr, "Buzz", nullptr, nullptr,
+       nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+       "FizzBuzz"}};
+
+  ostringstream oss{};
+  oss << "1";
+
+  for (int i{2}; i <= number; ++i) {
+    oss << ' '
+        << (i % 15 == 0 ? fizz_buzz_dict.at(15)
+                        : (i % 5 == 0 ? fizz_buzz_dict.at(5)
+                                      : (i % 3 == 0 ? fizz_buzz_dict.at(3)
+                                                    : itoa(i, nullptr, 10))));
+  }
+
+  return oss.str();
+}
+// ***** end of 3rd solution *****
+
+// ***** beginning of 4th solution *****
+using process_correct_case_fn = ostream& (*)(ostream&, const int);
+
+inline ostream& get_number(ostream& os, const int number) {
+  return os << number;
+}
+
+inline ostream& get_fizz(ostream& os, const int) {
+  return os << "Fizz";
+}
+
+inline ostream& get_buzz(ostream& os, const int) {
+  return os << "Buzz";
+}
+
+inline ostream& get_fizz_buzz(ostream& os, const int) {
+  return os << "FizzBuzz";
+}
+
+string FizzBuzz_v4(const int number) {
+  constexpr const process_correct_case_fn fizz_buzz_funcs[9]{
+      get_number, nullptr, nullptr, get_fizz,     nullptr,
+      get_buzz,   nullptr, nullptr, get_fizz_buzz};
+
+  ostringstream oss{};
+  oss << "1";
+
+  for (int i{2}; i <= number; ++i) {
+    const int index{(i % 3 == 0) * 3 + (i % 5 == 0) * 5};
+    oss << ' ';
+    fizz_buzz_funcs[index](oss, i);
+  }
+
+  return oss.str();
+}
+// ***** end of 4th solution *****
 
 int main() {
-  // cout << FizzBuzz(gets(stdin));
-  cout << FizzBuzz(16) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7 8
-                                 // Fizz Buzz 11 Fizz 13 14 FizzBuzz 16"
-  cout << FizzBuzz(3) << '\n';   // expected output: "1 2 Fizz"
-  cout << FizzBuzz(8) << '\n';   // expected output: "1 2 Fizz 4 Buzz Fizz 7 8"
+  // cout << FizzBuzz_v4(gets(stdin));
+  cout << FizzBuzz_v4(16) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7
+                                    // 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16"
+  cout << FizzBuzz_v4(3) << '\n';   // expected output: "1 2 Fizz"
+  cout << FizzBuzz_v4(8)
+       << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7 8"
 
   return 0;
 }
