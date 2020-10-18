8e13274e4c24461eae900b182a509e49a2e94146
Attila Budai <atib1980@gmail.com>
2020-10-18 21:15:04 +0200

    simplified Two_Sum_v3 fn, added Two_Sum_v4 fn

--- 1_easy_difficulty_level/46_two_sum.cpp
+++ 1_easy_difficulty_level/46_two_sum.cpp
@@ -108,74 +108,34 @@ ForwardIter optimal_bsearch(ForwardIter first,
   return value == *first ? first : orig_last;
 }
 
-bool check_if_found_number_has_been_used(
-    const size_t index,
-    const vector<int>& sorted_numbers,
-    unordered_set<size_t>& ignored_indices) {
-  bool illegal_index = false;
-
-  if (0U != ignored_indices.count(index)) {
-    const int target_number{sorted_numbers.at(index)};
-    illegal_index = true;
-
-    for (size_t x{index}, y{index + 1}; x > 0U || y < sorted_numbers.size();) {
-      if (target_number == sorted_numbers[x - 1] &&
-          0U == ignored_indices.count(x - 1)) {
-        ignored_indices.emplace(x - 1);
-        illegal_index = false;
-        break;
-      } else if (target_number == sorted_numbers[y] &&
-                 0U == ignored_indices.count(y)) {
-        ignored_indices.emplace(y);
-        illegal_index = false;
-        break;
-      } else if (target_number != sorted_numbers[x - 1] &&
-                 target_number != sorted_numbers[y])
-        break;
-
-      if (x > 0U)
-        --x;
-    }
-  } else {
-    ignored_indices.emplace(index);
-  }
-
-  return illegal_index;
-}
-
-string TwoSum_v3(const int* arr, const size_t arr_size) {
+string TwoSum_v3(int* arr, const size_t arr_size) {
   if (arr_size < 3U)
     return "-1";
 
   const auto target_num{arr[0]};
-  vector<int> sorted_numbers(arr + 1, arr + arr_size);
-  sort(begin(sorted_numbers), end(sorted_numbers));
-  unordered_set<size_t> ignored_indices{};
+  sort(arr + 1, arr + arr_size);
+  unordered_set<int> ignored_numbers{};
 
   ostringstream oss{};
 
-  for (size_t i{}; i < sorted_numbers.size(); ++i) {
-    const auto second_number{target_num - sorted_numbers[i]};
+  for (size_t i{1}; i < arr_size; ++i) {
+    if (0U != ignored_numbers.count(arr[i]))
+      continue;
+    ignored_numbers.emplace(arr[i]);
+    const auto second_number{target_num - arr[i]};
 
-    ignored_indices.emplace(i);
-
-    if (second_number < sorted_numbers[i]) {
-      const auto iter = optimal_bsearch(
-          cbegin(sorted_numbers), cbegin(sorted_numbers) + i, second_number);
-      if (iter != cbegin(sorted_numbers) + i &&
-          !check_if_found_number_has_been_used(
-              static_cast<size_t>(iter - cbegin(sorted_numbers)),
-              sorted_numbers, ignored_indices)) {
-        oss << second_number << ',' << sorted_numbers[i] << ' ';
+    if (second_number < arr[i]) {
+      const auto iter = optimal_bsearch(arr + 1, arr + i, second_number);
+      if (iter != arr + i) {
+        ignored_numbers.emplace(second_number);
+        oss << second_number << ',' << arr[i] << ' ';
       }
     } else {
-      const auto iter = optimal_bsearch(cbegin(sorted_numbers) + i + 1,
-                                        cend(sorted_numbers), second_number);
-      if (iter != cend(sorted_numbers) &&
-          !check_if_found_number_has_been_used(
-              static_cast<size_t>(iter - cbegin(sorted_numbers)),
-              sorted_numbers, ignored_indices)) {
-        oss << sorted_numbers[i] << ',' << second_number << ' ';
+      const auto iter =
+          optimal_bsearch(arr + i + 1, arr + arr_size, second_number);
+      if (iter != arr + arr_size) {
+        ignored_numbers.emplace(second_number);
+        oss << arr[i] << ',' << second_number << ' ';
       }
     }
   }
@@ -189,26 +149,56 @@ string TwoSum_v3(const int* arr, const size_t arr_size) {
   return "-1";
 }
 
+string TwoSum_v4(int* arr, const size_t arr_size) {
+  if (arr_size < 3U)
+    return "-1";
+
+  const auto target_num{arr[0]};
+  sort(arr + 1, arr + arr_size);
+
+  ostringstream oss{};
+
+  for (size_t i{1}, j{arr_size - 1}; i < j;) {
+    if (arr[i] + arr[j] == target_num) {
+      oss << arr[i] << ',' << arr[j] << ' ';
+      ++i;
+      --j;
+    } else if (arr[i] + arr[j] < target_num) {
+      ++i;
+    } else {
+      --j;
+    }
+  }
+
+  if (oss.tellp() != 0) {
+    string result{oss.str()};
+    result.pop_back();
+    return result;
+  }
+
+  return "-1";
+}
+
 int main() {
-  // const int A[] = gets(stdin);
-  // cout << TwoSum_v2(A, sizeof(A)/sizeof(*A));
-  const int b[]{7, 3, 5, 2, -4, 8, 11};
+  // int A[] = gets(stdin);
+  // cout << TwoSum_v4(A, sizeof(A)/sizeof(*A));
+  int b[]{7, 3, 5, 2, -4, 8, 11};
   cout << TwoSum_v3(b, sizeof(b) / sizeof(*b))
        << '\n';  // expected output: "5,2 -4,11"
-  const int c[]{17, 4, 5, 6, 10, 11, 4, -3, -5, 3, 15, 2, 7};
+  int c[]{17, 4, 5, 6, 10, 11, 4, -3, -5, 3, 15, 2, 7};
   cout << TwoSum_v3(c, sizeof(c) / sizeof(*c))
        << '\n';  // expected output: "6,11 10,7 15,2"
-  const int d[]{7, 6, 4, 1, 7, -2, 3, 12};
+  int d[]{7, 6, 4, 1, 7, -2, 3, 12};
   cout << TwoSum_v3(d, sizeof(d) / sizeof(*d))
        << '\n';  // expected output: "6,1 4,3"
-  const int e[]{6, 2};
+  int e[]{6, 2};
   cout << TwoSum_v3(e, sizeof(e) / sizeof(*e))
        << '\n';  // expected output: "-1"
-  const int f[]{100, 90, 90, 90, 90, 11};
+  int f[]{100, 90, 90, 90, 90, 11};
   cout << TwoSum_v3(f, sizeof(f) / sizeof(*f))
        << '\n';  // expected output: "-1"
-  const int g[]{4, 5, 2, 1};
-  cout << TwoSum_v3(g, sizeof(g) / sizeof(*g))
+  int g[]{4, 5, 2, 1};
+  cout << TwoSum_v4(g, sizeof(g) / sizeof(*g))
        << '\n';  // expected output: "-1"
 
   return 0;
