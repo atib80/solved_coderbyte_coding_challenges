624cac4e8fa67c1b683e58f9570f789b11f21ee6
Attila Budai <atib1980@gmail.com>
2021-02-26 04:51:31 -0800

    couple of small changes

--- 1_easy_difficulty_level/02_first_factorial.cpp
+++ 1_easy_difficulty_level/02_first_factorial.cpp
@@ -25,99 +25,6 @@ Output: 40320
 
 using namespace std;
 
-class big_integer {
-  using byte = unsigned char;
-  using number_base = stl::helper::number_base;
-
-  const number_base base_;
-  vector<byte> digits_;
-  string number_str_;
-  string_view number_str_view_;
-  bool is_negative_number_;
-  size_t first_;
-  size_t last_;
-
- public:
-  explicit big_integer(const char* number,
-                       const number_base base = number_base::decimal)
-      : base_{base} {
-    store_number(number);
-  }
-
-  explicit big_integer(const string& number,
-                       const number_base base = number_base::decimal)
-      : big_integer(number.c_str(), base) {}
-
-  big_integer(const big_integer& rhs) : base_{rhs.base_} {
-    store_number(rhs.get_big_integer().c_str());
-  }
-
-  number_base get_big_integer_number_base() const { return base_; }
-
-  string get_big_integer() const {
-    return string(cbegin(number_str_) + first_, cbegin(number_str_) + last_);
-  }
-
-  explicit operator string() const { return get_big_integer(); }
-
-  explicit operator const char*() const { return &number_str_[first_]; }
-
- private:
-  void store_number(const char* number) {
-    if (number != nullptr && stl::helper::len(number) > 0) {
-      number_str_ = stl::helper::trim(number);
-    } else {
-      throw invalid_argument{
-          "Input number string does not contain a valid number!"};
-    }
-
-    digits_.resize(128U);
-
-    first_ = 0U;
-
-    if (number_str_[0] == '-' || number_str_[0] == '+') {
-      is_negative_number_ = number_str_[0] == '-';
-      first_++;
-    }
-
-    if (base_ == number_base::binary) {
-      if ('b' == number_str_[0] || 'B' == number_str_[0]) {
-        first_++;
-
-      } else if ('0' == number_str_[0] &&
-                 ('b' == number_str_[1] || 'B' == number_str_[1])) {
-        first_ += 2;
-      }
-
-    } else if (base_ == number_base::octal) {
-      if ('0' == number_str_[0] || 'o' == number_str_[0] ||
-          'O' == number_str_[0]) {
-        first_++;
-      }
-
-    } else if (base_ == number_base::decimal) {
-      while (isspace(number_str_[first_])) {
-        first_++;
-      }
-
-      if (number_str_[first_] < '0' || number_str_[first_] > '9') {
-        throw invalid_argument{
-            "Input number string does not contain a valid number!"};
-      }
-
-      last_ = number_str_.find_first_not_of("0123456789", first_ + 1);
-
-    } else if (base_ == number_base::hexadecimal) {
-      if ('0' == number_str_[0] &&
-          ('x' == number_str_[1] || 'X' == number_str_[1])) {
-        first_ += 2;
-      }
-    }
-
-    number_str_view_ = {&number_str_[first_], last_ - first_};
-  }
-};
-
 template <size_t N = 20>
 struct precalculated_factorials {
   uint64_t factorials[N + 1];
@@ -130,6 +37,9 @@ struct precalculated_factorials {
   }
 
   constexpr uint64_t calculate_factorial(uint64_t n) const {
+    if (n <= 2U)
+      return 0U == n ? 1U : n;
+
     uint64_t result{n};
 
     while (--n > 1)
@@ -148,29 +58,26 @@ struct precalculated_factorials {
 static constexpr precalculated_factorials<> factorials;
 
 uint64_t FirstFactorial_v1(uint64_t num) {
-  if (!num)
-    return 1;
-
-  if (num < 3)
-    return num;
+  if (num <= 2ULL)
+    return 0ULL == num ? 1ULL : num;
 
   uint64_t result{num};
 
-  while (num > 2)
+  while (num > 2ULL)
     result *= --num;
 
   return result;
 }
 
 uint64_t FirstFactorial_v2(const uint64_t num) {
-  if (num < 2LL)
-    return 1LL;
+  if (num < 2ULL)
+    return 1ULL;
 
   return num * FirstFactorial_v2(num - 1);
 }
 
 uint64_t FirstFactorial_v3(const uint64_t num) {
-  if (num <= 20)
+  if (num <= 20ULL)
     return factorials.get_factorial_of_specified_number(num);
   return FirstFactorial_v1(num);
 }
