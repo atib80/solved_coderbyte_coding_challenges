211c79f9396d54455c141821d204f1147666231e
Attila Budai <atib1980@gmail.com>
2021-03-20 15:23:49 +0100

    added vowel_substring.cpp & 4 diff. solutions to fizzbuzz

--- 1_easy_difficulty_level/85_fizzbuzz.cpp
+++ 1_easy_difficulty_level/85_fizzbuzz.cpp
@@ -30,28 +30,9 @@ Output: "1 2 Fizz 4 Buzz Fizz 7 8"
 using namespace std;
 
 // ***** beginning of 1st solution *****
-string FizzBuzz_v1(const int num) {
+string FizzBuzz_v1(const int number) {
   ostringstream oss{};
-  oss << "1";
-  for (int i{2}; i <= num; ++i) {
-    if (i % 3 == 0 && i % 5 == 0)
-      oss << " FizzBuzz";
-    else if (i % 3 == 0)
-      oss << " Fizz";
-    else if (i % 5 == 0)
-      oss << " Buzz";
-    else
-      oss << ' ' << i;
-  }
-
-  return oss.str();
-}
-// ***** end of 1st solution *****
-
-// ***** beginning of 2nd solution *****
-string FizzBuzz_v2(const int number) {
-  ostringstream oss{};
-  oss << "1";
+  oss << '1';
 
   for (int i{2}; i <= number; ++i) {
     const bool divisible_by_3{0 == i % 3};
@@ -66,24 +47,53 @@ string FizzBuzz_v2(const int number) {
 
   return oss.str();
 }
+// ***** end of 1st solution *****
+
+// ***** beginning of 2nd solution ****
+static const char* fizz_buzz_dict[]{nullptr, nullptr, nullptr,
+                                    " Fizz", nullptr, " Buzz",
+                                    nullptr, nullptr, " FizzBuzz"};
+string FizzBuzz_v2(const int number) {
+  ostringstream oss{};
+  oss << '1';
+
+  for (int i{2}; i <= number; ++i) {
+    const int index{(0 == i % 3) * 3 + (0 == i % 5) * 5};
+    if (0 != index)
+      oss << fizz_buzz_dict[index];
+    else
+      oss << ' ' << i;
+  }
+
+  return oss.str();
+}
+
 // ***** end of 2nd solution *****
 
 // ***** beginning of 3rd solution *****
 string FizzBuzz_v3(const int number) {
-  constexpr const array<const char*, 16> fizz_buzz_dict{
-      {nullptr, nullptr, nullptr, "Fizz", nullptr, "Buzz", nullptr, nullptr,
-       nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-       "FizzBuzz"}};
-
   ostringstream oss{};
-  oss << "1";
+  oss << '1';
 
   for (int i{2}; i <= number; ++i) {
-    oss << ' '
-        << (i % 15 == 0 ? fizz_buzz_dict.at(15)
-                        : (i % 5 == 0 ? fizz_buzz_dict.at(5)
-                                      : (i % 3 == 0 ? fizz_buzz_dict.at(3)
-                                                    : itoa(i, nullptr, 10))));
+    const int index{(0 == i % 3) * 3 + (0 == i % 5) * 5};
+    switch (index) {
+      default:
+        oss << ' ' << i;
+        break;
+
+      case 3:
+        oss << " Fizz";
+        break;
+
+      case 5:
+        oss << " Buzz";
+        break;
+
+      case 8:
+        oss << " FizzBuzz";
+        break;
+    }
   }
 
   return oss.str();
@@ -91,49 +101,60 @@ string FizzBuzz_v3(const int number) {
 // ***** end of 3rd solution *****
 
 // ***** beginning of 4th solution *****
-using process_correct_case_fn = ostream& (*)(ostream&, const int);
-
-inline ostream& get_number(ostream& os, const int number) {
-  return os << number;
+inline ostream& print_number(ostream& os, const int number) {
+  os << ' ' << number;
+  return os;
 }
 
-inline ostream& get_fizz(ostream& os, const int) {
-  return os << "Fizz";
+inline ostream& print_fizz(ostream& os, const int) {
+  os << " Fizz";
+  return os;
 }
 
-inline ostream& get_buzz(ostream& os, const int) {
-  return os << "Buzz";
+inline ostream& print_buzz(ostream& os, const int) {
+  os << " Buzz";
+  return os;
 }
 
-inline ostream& get_fizz_buzz(ostream& os, const int) {
-  return os << "FizzBuzz";
+inline ostream& print_fizz_buzz(ostream& os, const int) {
+  os << " FizzBuzz";
+  return os;
 }
 
 string FizzBuzz_v4(const int number) {
-  constexpr const process_correct_case_fn fizz_buzz_funcs[9]{
-      get_number, nullptr, nullptr, get_fizz,     nullptr,
-      get_buzz,   nullptr, nullptr, get_fizz_buzz};
+  using process_correct_case_fn = ostream& (*)(ostream&, const int);
+
+  static const process_correct_case_fn fizz_buzz_funcs[9]{
+      print_number, nullptr, nullptr, print_fizz,     nullptr,
+      print_buzz,   nullptr, nullptr, print_fizz_buzz};
 
   ostringstream oss{};
-  oss << "1";
+  oss << '1';
 
   for (int i{2}; i <= number; ++i) {
     const int index{(i % 3 == 0) * 3 + (i % 5 == 0) * 5};
-    oss << ' ';
     fizz_buzz_funcs[index](oss, i);
   }
 
   return oss.str();
 }
+
 // ***** end of 4th solution *****
 
 int main() {
   // cout << FizzBuzz_v4(gets(stdin));
-  cout << FizzBuzz_v4(16) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7
-                                    // 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16"
-  cout << FizzBuzz_v4(3) << '\n';   // expected output: "1 2 Fizz"
-  cout << FizzBuzz_v4(8)
-       << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7 8"
-
+  // cout << FizzBuzz_v4(16) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz
+  // 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16"
+  // cout << FizzBuzz_v4(3) << '\n';   // expected output: "1 2 Fizz"
+  // cout << FizzBuzz_v4(8) << '\n';   // expected output: "1 2 Fizz 4 Buzz Fizz
+  // 7 8"
+  cout << FizzBuzz_v1(15) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7
+                                    // 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
+  cout << FizzBuzz_v2(15) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7
+                                    // 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
+  cout << FizzBuzz_v3(15) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7
+                                    // 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
+  cout << FizzBuzz_v4(15) << '\n';  // expected output: "1 2 Fizz 4 Buzz Fizz 7
+                                    // 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
   return 0;
 }
---
+++ 1_easy_difficulty_level/vowel_substring.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+/*
+ * Description:
+ * Given a string 's' and a whole number 'k' find the first substring of length
+ k that contains the most vowels (number of vowels <= k). If you cannot find any
+ substring of length k that contains at least 1 vowel return the string "Not
+ found!"
+
+ *
+ * Complete the 'findSubstring' function below.
+ *
+ *
+ * The function is expected to return a string.
+ * The function accepts the following parameters:
+ *  1. string s
+ *  2. int k
+ */
+
+string findSubstring(const string& s, size_t k) {
+  static int allowed_vowels[128]{};
+  allowed_vowels['a'] = allowed_vowels['e'] = allowed_vowels['i'] =
+      allowed_vowels['o'] = allowed_vowels['u'] = 1;
+
+  // static const unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u'};
+  size_t start_pos{string::npos};
+
+  k = min(k, s.length());
+
+  size_t max_vowel_count =
+      count_if(cbegin(s), cbegin(s) + k, [&](const char ch) {
+        return 1 == allowed_vowels[static_cast<size_t>(ch)];
+      });
+  size_t vowel_count{max_vowel_count}, offset{1};
+
+  if (0U != max_vowel_count)
+    start_pos = 0U;
+  else
+    offset = k;
+
+  if (k == max_vowel_count)
+    return s.substr(0, k);
+
+  for (size_t i{offset}; i + k <= s.length(); ++i) {
+    if (1 == allowed_vowels[static_cast<size_t>(s[i - 1])])
+      --vowel_count;
+    if (1 == allowed_vowels[static_cast<size_t>(s[i + k - 1])])
+      ++vowel_count;
+    if (vowel_count > max_vowel_count) {
+      if (k == vowel_count)
+        return s.substr(i, k);
+      max_vowel_count = vowel_count;
+      start_pos = i;
+    }
+  }
+
+  if (string::npos != start_pos)
+    return s.substr(start_pos, k);
+
+  return "Not found!";
+}
+
+int main() {
+  cout << findSubstring("caberqiitefg", 5) << '\n';  // expected output: "erqii"
+  cout << findSubstring("aeiouia", 3) << '\n';       // expected output: "aei"
+  cout << findSubstring("qwdftr", 2) << '\n';  // expected output: "Not found!"
+
+  return 0;
+}
